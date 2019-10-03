7c88edf85af2dae475bb3c776349bc7f3a7c6484
baeha <baehayoung0214@gmail.com>
2019-10-03 22:11:02 +0900

    I hate err :(

--- forlib/collection/file_open.py
+++ forlib/collection/file_open.py
@@ -1,35 +1,36 @@
 import Evtx.Evtx as evtx
 import zipfile
+import os
+from os import listdir
+from PIL import Image
+import forlib.collection.signature as sig
+
 
 def signature_db(path):
-    file_extension = path.split('.')[1]
-    return str(file_extension)
+    return sig.sig_check(path)
 
-def extension_file_open(extension, path):
-    if extension == 'evtx':
-        return evtx_open(path)
-    elif extension == 'regf':
-        return reg_open(path)
-    elif extension == 'zip':
-        return zip_open(path)
-    elif extension == 'jpeg':
-        return jpeg_open(path)
-    elif extension == 'thum':
-        return thum_open(path)
 
 def evtx_open(path):
     return evtx.Evtx(path)
 
-def reg_open(path):
-    return path
 
 def zip_open(path):
     file = open(path, 'rb')
     z = zipfile.ZipFile(file, 'r')
     return z
 
-def jpeg_open(path):
-    return path
 
-def thum_open(path):
-    return path
+def jpeg_open(path):
+    return Image.open(path)
+
+
+def systemp_open(path):
+    return [f for f in listdir(path)]
+
+
+def binary_open(path):
+    return open(path, 'rb')
+
+
+def normal_file_oepn(path):
+    return open(path, 'r')
