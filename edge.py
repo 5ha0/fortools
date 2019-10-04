9e33b3838d71451db20b6166707d330cf261fe74
unknown <cjh0313c@gmail.com>
2019-10-04 17:30:32 +0900

    test

--- edge.py
+++
@@ -1,5 +0,0 @@
-import pyesedb
-
-def edge_open(path):
-    open_edge_file=open(path,"rb")
-    return open_edge_file
--- test.py
+++
@@ -1,8 +0,0 @@
-from log import *
-
-file = file_open('Application.evtx')
-
-#file = evtx_log
-with file as log:
-    for x in log.records():
-        print(x)
