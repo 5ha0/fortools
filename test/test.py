2feff2ab1eecc4734b3477273b5f944a77e49888
liberte97 <liberte97@naver.com>
2019-10-09 01:13:19 +0900

    test.py modify

--- test/test.py
+++ test/test.py
@@ -1,7 +1,10 @@
 from forlib.fortools import Log
-from forlib.fortools import file
-from forlib.fortools import unknown
-
+from forlib.fortools import Files
+from forlib.fortools import Unknown
+from forlib.fortools import Registry
+from forlib.fortools import System_temp
+import os, datetime
+'''
 zip_file = file.file_open('./zip_test.zip')
 zip_info = zip_file.infolist()
 file_info = []
@@ -12,12 +15,7 @@ for i in range(zip_info.__len__()):
 
 #evtx_file = log.file_open('C:\Windows\System32\winevt\Logs\Application.evtx')
 #evtx_file.get_event_ID(10)
-=======
-from forlib.fortools import registry
-from forlib.fortools import unknown
-from forlib.fortools import system_temp
-import os, datetime
-
+'''
 # user_profile = os.environ['USERPROFILE']
 # systemp_file = system_temp.file_open(user_profile+'\Local Settings\Temp')
 # for i in range(systemp_file.__len__()):
@@ -31,14 +29,14 @@ import os, datetime
 #     file_info.append(zip_info[i])
 #     print(file_info[i])
 
-'''
-reg_file = registry.file_open("test/NTUSER.DAT")
+reg_file = Registry.file_open("test/NTUSER.DAT")
 print("==================[+]registry microsoft information==================\n")
-reg_info = reg_file.get_find_key("microsoft")
-print("\n==================[+]registry recent file-----need decode modify==================\n")
-reg_recent = reg_file.recent_docs()
+reg_info = reg_file.get_find_key("Windows Mail")
+reg_info2 = reg_file.get_find_key("microsoft")
+# print("\n==================[+]registry recent file-----need decode modify==================\n")
+#reg_recent = reg_file.recent_docs()
 '''
-
 evtx_file = log.file_open('C:\Windows\System32\winevt\Logs\Application.evtx')
 #evtx_file.show_all_record()
 evtx_file.eventID(15)
+'''
