bb9daef35dca90c4078f67eaa8059cde14058a92
gut27 <52551159+gut27@users.noreply.github.com>
2019-10-16 19:56:36 +0900

    Update prefetch_analysis.py

--- prefetch_analysis.py
+++ prefetch_analysis.py
@@ -12,91 +12,89 @@ import decompress1
 
 
 class Prefetch:
-    global file
-    global version
+    def __init__(self,file):
+        self.file = file
     
-    def pf_open(path):
-        global file
-        global version
-        
-        file = open(path, 'rb')
+    def pf_open(self, path):    
+        self.file = open(path, 'rb')
         if file.read(3) == b'MAM':
             file.close()
             decompressed = decompress1.decompress(path)
-            file = open('', 'wb')
-            file.write(decompressed)
-            print(decompressed)
 
-        file.seek(0)
-        version = struct.unpack_from('I', file.read(4))[0]
+            dirname = os.path.dirname(path)
+            basename = os.path.basename(path)
+            base = os.path.splitext(basename)
+            basename = base[0]
+            exetension = base[-1]
+            
+            self.file = open(dirname+'\\'+basename+'-1'+exetension,'wb')
+            self.file.write(decompressed)
+            self.file.close()
+            
+        self.file = open(dirname+'\\'+basename+'-1'+exetension,'rb')
+        version = struct.unpack_from('I', self.file.read(4))[0]
             
         if version != 23 and version != 30:
             print ('error: not supported version')
 
-        signature = file.read(4)
-        if signature != 'SCCA':
+        signature = self.file.read(4)
+        print(signature)
+        if signature != b'SCCA':
             print('not prefetch file')
         
         print('Success file open')
-        return file
+        return self.file
 
 
     def dt_from_win32_ts(timestamp):
         WIN32_EPOCH = datetime(1601, 1, 1)
         return WIN32_EPOCH + timedelta(microseconds=timestamp // 10, hours=9)  
 
-    def pf_size():
-        global file
-        global version
-        file.seek(12)
-        size = struct.unpack_from('I', file.read(4))[0]
+    def pf_size(self):
+        self.file.seek(12)
+        size = struct.unpack_from('I', self.file.read(4))[0]
         print(size)
+        return size
         
-    def pf_last_run_time():
-        file.seek(128)
-        time = struct.unpack_from("<Q", file.read(8))[0]
+    def pf_last_run_time(self):
+        self.file.seek(128)
+        time = struct.unpack_from("<Q", self.file.read(8))[0]
         time = '%016x' %time
         time = int(time,16)/10.
         last_run_time =  datetime(1601, 1, 1) + timedelta(microseconds=time)+timedelta(hours=9)  
         print("File Last Run Time: " + str(last_run_time) +' UTC+9:00')
 
-    def pf_create_time(path):
+    def pf_create_time(self, path):
         time = datetime.fromtimestamp(os.path.getctime(path))
         print ('file create time: '+str(time) +' UTC+9:00')
 
-    def pf_write_time(path):
+    def pf_write_time(self, path):
         time = datetime.fromtimestamp(os.path.getmtime(path))
         print ('file create time: '+ str(time) +' UTC+9:00')
 
-    def pf_num_launch():
-        global file
-        global version
+    def pf_num_launch(self):
         if version == 23:
-            file.seek(152)
-            print('File Run Count:'+ str(struct.unpack_from('I', file.read(4))[0]))
+            self.file.seek(152)
+            print('File Run Count:'+ str(struct.unpack_from('I', self.file.read(4))[0]))
         elif version ==30:
-            file.seek(208)
-            print('File Run Count:'+ str(struct.unpack_from('I', file.read(4))[0]))
+            self.file.seek(208)
+            print('File Run Count:'+ str(struct.unpack_from('I', self.file.read(4))[0]))
             
     def pf_file_list():
-        global file
-        global version
-        file.seek(100)
-        file_list_offset=struct.unpack_from('I', file.read(4))[0]
-        file_list_size=struct.unpack_from('I', file.read(4))[0]
+        self.file.seek(100)
+        file_list_offset=struct.unpack_from('I', self.file.read(4))[0]
+        file_list_size=struct.unpack_from('I', self.file.read(4))[0]
         resource = []
-        file.seek(file_list_offset)
-        filenames = file.read(file_list_size)
+        self.file.seek(file_list_offset)
+        filenames = self.file.read(file_list_size)
         filenames = filenames.decode('cp1252')
         for i in filenames.split('\x00\x00'):
             resource.append(i.replace('\x00',''))
-        count = 1
+
         for i in resource:
-            print('NO{}: {}'.format(count,i))
             count += 1
-
-##class favorite:
-##    def time_stamp:
-
-
-
+            pf_obj = {
+                "num" : count,
+                "ref_file" : i
+            }
+            print(json.dumps(pf_obj))
