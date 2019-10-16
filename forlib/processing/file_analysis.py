d915b2a14f1172600662592b9dbcb658a7489c05
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-10-17 03:55:05 +0900

    Update file_analysis.py

--- forlib/processing/file_analysis.py
+++ forlib/processing/file_analysis.py
@@ -1,100 +1,126 @@
 import json
+from os import listdir
 from lxml import etree
 import datetime
 from Registry import Registry
+import forlib.processing.reg_analysis as regf
+import forlib.processing.prefetch_analysis as pf
+import forlib.processing.filesystem_analysis as fs
 
-class Evtx_analysis():
-    evtx_json = []
+class LogAnalysis:
+    class EvtxAnalysis:
+        evtx_json = []
 
-    def __init__(self, file):
-        self.evtx_file = file
-        self.evtx_json = self.make_JSON()
 
-    def show_all_record(self):
-        for i in self.evtx_json:
-            print(i)
+        def __init__(self, file):
+            self.evtx_file = file
+            self.evtx_json = self.make_JSON()
 
-    def make_XML(self):
-        if self.evtx_file.number_of_records > 0:
+        def show_all_record(self):
+            for i in self.evtx_json:
+                print(i)
+
+        def make_XML(self):
+            if self.evtx_file.number_of_records > 0:
+                for i in range(0, len(self.evtx_file.records)):
+                    self.evtx_file.records[i].get_xml_string()
+
+        def make_JSON(self):
+            json_list = []
             for i in range(0, len(self.evtx_file.records)):
-                self.evtx_file.records[i].get_xml_string()
+                log_obj = dict()
+                log_obj["eventID"] = self.evtx_file.records[i].get_event_identifier()
+                log_obj["create Time"] = str(self.evtx_file.records[i].get_creation_time())
+                log_obj["level"] = self.evtx_file.records[i].get_event_level()
+                log_obj["source"] = self.evtx_file.records[i].get_source_name()
+                log_obj["computer Info"] = self.evtx_file.records[i].get_computer_name()
+                log_obj["SID"] = self.evtx_file.records[i].get_user_security_identifier()
+                log_num_object = dict()
+                log_num_object["no"+str(i)] = log_obj
+                json_list.append(log_num_object)
+            return json_list
 
-    def make_JSON(self):
-        json_list = []
-        for i in range(0, len(self.evtx_file.records)):
-            log_obj = dict()
-            log_obj["eventID"] = self.evtx_file.records[i].get_event_identifier()
-            log_obj["create Time"] = str(self.evtx_file.records[i].get_creation_time())
-            log_obj["level"] = self.evtx_file.records[i].get_event_level()
-            log_obj["source"] = self.evtx_file.records[i].get_source_name()
-            log_obj["computer Info"] = self.evtx_file.records[i].get_computer_name()
-            log_obj["SID"] = self.evtx_file.records[i].get_user_security_identifier()
-            json_list.append(log_obj)
-        return json_list
+        def eventID(self, num):
+            for i in range(0, len(self.evtx_json)):
+                if self.evtx_json[i]['no'+str(i)]['eventID'] == num:
+                    print(self.evtx_json[i])
 
-    def eventID(self, num):
-        for i in self.evtx_json:
-            if i['eventID'] == num:
+        def level(self, num):
+            for i in  range(0, len(self.evtx_json)):
+                if self.evtx_json[i]['no'+str(i)]['level'] == num:
+                    print(self.evtx_json[i])
+
+        def xml_with_num(self, num):
+            print(self.evtx_file.records[num].get_xml_string())
+
+    class WebLogAnalysis:
+        def __init__(self, file):
+            self.file = file
+            self.weblog_json = self.make_json()
+
+        def make_json(self):
+            idx = 0
+            json_list = []
+            while True:
+                line = self.file.readline()
+                if line == '':
+                    break
+                elif line[0] == '#' and line[0:7] != '#Fields':
+                    continue
+                if line[0:7] == '#Fields':
+                    fields = line.split(' ')
+                else:
+                    log_line = line.split()
+                    log_obj = dict()
+                    for i in range(1, len(fields)):
+                        log_obj[fields[i]] = log_line[i-1]
+                    json_list.append({'no'+str(idx): log_obj})
+                    idx = idx+1
+            return json_list
+
+        def show_all_record(self):
+            for i in self.weblog_json:
                 print(i)
 
-    def level(self, num):
-        for i in self.evtx_json:
-            if i['level'] == num:
-                print(i)
+        def date(self, date):
+            for i in range(0, len(self.weblog_json)):
+                if self.weblog_json[i]['no'+str(i)]['date'] == date:
+                    print(self.weblog_json[i])
 
-
-class Log_analysis():
-    def __init__(self, file):
-        self.file = file
-
-    def make_json(self):
-        line = self.file.readline()
-        log_line = line.split()
-        log_obj = dict()
-        log_obj["host"] = log_line[0]
-        log_obj["indent"] = log_line[1]
-        log_obj["AuthUser"] = log_line[2]
-        log_obj["Date"] = log_line[3]
-        log_obj["Request"] = log_line[4]
-        log_obj["Status"] = log_line[5]
-        log_obj["Bytes"] = log_line[6]
-        return json.dumps(log_obj, ensure_ascii=False, indent='\t')
-
-
-class Files_analysis():
+class FileAnalysis():
     def __init__(self, file):
         self.file = file
 
         
-class Reg_analysis():
+class RegAnalysis:
+    def NTUSER(file):
+        return regf.NTAnalysis(file)
+
+    def SYSTEM(file):
+        return regf.SYSAnalysis(file)
+
+class FSAnalysis:
+    def __init__(self):
+        self.file = file
+    
+    def DD(self):
+        return fs.DDAnalysis(self.file)
+
+class System_temp_analysis():
     def __init__(self, file):
-        self.reg = file
+        self._file = file
 
-    def rec(self, key, find_path, find_val):
-        find_path(key, find_val)
-        for subkey in key.subkeys():
-            self.rec(subkey, find_path, find_val)
+    def get_temp(self, path):
+        files = [f for f in listdir(path)]
+        for i in range(files.__len__()):
+            print(files[i])
 
-    def find_path(self, key, find_val):
-        for value in [v.value() for v in key.values()
-                        if v.value_type() == Registry.RegSZ
-                        or v.value_type() == Registry.RegExpandSZ]:
-                        if find_val in value:
-                            reg_key_obj = {
-                                "find_keyword" : find_val,
-                                "key" : key.path()
-                            }
-                            print (print(json.dumps(reg_key_obj)))
-
-    def get_find_key(self, keyword):
-        self.rec(self.reg.root(), self.find_path, keyword)
-
-    def recent_docs(self):
-        recent = self.reg.open("SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs")
-        for i, v in enumerate(recent.values()):
-            reg_obj  = {
-                    "time" : str(recent.timestamp()),
-                    "name" : v.name(),
-                    "data" : v.value().decode('utf-16')}
-            print(json.dumps(reg_obj))
-            #print ('{} > {} : {}'.format(recent.timestamp(), v.name(), v.value().decode('utf-16')))
+        file_count = files.__len__()
+        file_size = files.__sizeof__()
+        print("File Count : %d" %file_count)
+        print("File Size : %d" %file_size)
+'''
+class PrefetchAnalysis:
+    def __init__(self):
+        self.file = file
+'''
