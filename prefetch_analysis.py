8c582ff17edc99ecb5a13027718d291f41e5c216
gut27 <52551159+gut27@users.noreply.github.com>
2019-10-17 15:33:40 +0900

    Update prefetch_analysis.py

--- prefetch_analysis.py
+++ prefetch_analysis.py
@@ -17,8 +17,8 @@ class Prefetch:
     
     def pf_open(self, path):    
         self.file = open(path, 'rb')
-        if file.read(3) == b'MAM':
-            file.close()
+        if self.file.read(3) == b'MAM':
+            self.file.close()
             decompressed = decompress1.decompress(path)
 
             dirname = os.path.dirname(path)
@@ -63,15 +63,18 @@ class Prefetch:
         time = int(time,16)/10.
         last_run_time =  datetime(1601, 1, 1) + timedelta(microseconds=time)+timedelta(hours=9)  
         print("File Last Run Time: " + str(last_run_time) +' UTC+9:00')
+        return last_run_time
 
     def pf_create_time(self, path):
         time = datetime.fromtimestamp(os.path.getctime(path))
-        print ('file create time: '+str(time) +' UTC+9:00')
-
+        print ('File Create Time: '+str(time) +' UTC+9:00')
+        return time
+    
     def pf_write_time(self, path):
         time = datetime.fromtimestamp(os.path.getmtime(path))
-        print ('file create time: '+ str(time) +' UTC+9:00')
-
+        print ('File Create Time: '+ str(time) +' UTC+9:00')
+        return time
+    
     def pf_num_launch(self):
         if version == 23:
             self.file.seek(152)
@@ -79,7 +82,8 @@ class Prefetch:
         elif version ==30:
             self.file.seek(208)
             print('File Run Count:'+ str(struct.unpack_from('I', self.file.read(4))[0]))
-            
+        return num_launch
+    
     def pf_file_list():
         self.file.seek(100)
         file_list_offset=struct.unpack_from('I', self.file.read(4))[0]
@@ -94,7 +98,14 @@ class Prefetch:
         for i in resource:
             count += 1
             pf_obj = {
-                "num" : count,
-                "ref_file" : i
+                "Num" : count,
+                "Ref_file" : i
             }
             print(json.dumps(pf_obj))
+        return resourcce
+    
+##class favorite:
+##    def time_stamp:
+
+
+
