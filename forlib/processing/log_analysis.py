53af7a83a2f9fc1eb0c79ad71aac49336a649b3e
baeha <baehayoung0214@gmail.com>
2019-10-18 20:56:30 +0900

    add get_String()

--- forlib/processing/log_analysis.py
+++ forlib/processing/log_analysis.py
@@ -1,79 +1,13 @@
-class EvtxAnalysis:
-    evtx_json = []
-
-    def __init__(self, file):
-        self.evtx_file = file
-        self.evtx_json = self.make_json()
-        self.favorite = favorite(self.evtx_json)
-
-    def show_all_record(self):
-        for i in self.evtx_json:
-            print(i)
-
-    def make_xnl(self):
-        if self.evtx_file.number_of_records > 0:
-            for i in range(0, len(self.evtx_file.records)):
-                self.evtx_file.records[i].get_xml_string()
-
-    def make_json(self):
-        json_list = []
-        for i in range(0, len(self.evtx_file.records)):
-            log_obj = dict()
-            log_obj["eventID"] = self.evtx_file.records[i].get_event_identifier()
-            log_obj["create Time"] = str(self.evtx_file.records[i].get_creation_time())
-            log_obj["level"] = self.evtx_file.records[i].get_event_level()
-            log_obj["source"] = self.evtx_file.records[i].get_source_name()
-            log_obj["computer Info"] = self.evtx_file.records[i].get_computer_name()
-            log_obj["SID"] = self.evtx_file.records[i].get_user_security_identifier()
-            log_num_object = dict()
-            log_num_object["no" + str(i)] = log_obj
-            json_list.append(log_num_object)
-        return json_list
-
-    def eventid(self, num):
-        for i in range(0, len(self.evtx_json)):
-            if self.evtx_json[i]['no' + str(i)]['eventID'] == num:
-                print(self.evtx_json[i])
-
-    def level(self, num):
-        for i in range(0, len(self.evtx_json)):
-            if self.evtx_json[i]['no' + str(i)]['level'] == num:
-                print(self.evtx_json[i])
-
-    def xml_with_num(self, num):
-        print(self.evtx_file.records[num].get_xml_string())
-
-
-class favorite:
-    def __init__(self, json):
-        self.evtx_json = json
-
-    def logon(self): #detect valid logon record
-        EvtxAnalysis.eventid(self, 4624)
-
-    def remote(self): #detect remote logon record
-        EvtxAnalysis.eventid(self, 540)
-        EvtxAnalysis.eventid(self, 4776)
-
-    def marfind(self): #detect marfind record
-        print('user')
-
-    def user(self): #detect account info
-        print('user')
-
-    def authority(self): #detect time of elevation of authority
-        print('user')
-
-    def logon_type(self): #filtering based on logon type
-        print('user')
+import xmltodict
+import json
 
 
 class WebLogAnalysis:
     def __init__(self, file):
         self.file = file
-        self.weblog_json = self.make_json()
+        self.weblog_json = self.__make_json()
 
-    def make_json(self):
+    def __make_json(self):
         idx = 0
         json_list = []
         while True:
@@ -116,3 +50,107 @@ class WebLogAnalysis:
         for i in range(0, len(self.weblog_json)):
             if self.weblog_json[i]['no' + str(i)]['sc-status'] == status:
                 print(self.weblog_json[i])
+
+
+class EvtxAnalysis:
+    result = []
+
+    def __init__(self, file):
+        self.evtx_file = file
+        self.evtx_json = self.__make_json()
+        self.favorite = Favorite(self.evtx_json)
+
+    def show_all_record(self):
+        for i in self.evtx_json:
+            print(i)
+            self.result.append(i)
+        return self.result
+
+    def get_string(self):
+        json_list = []
+        if self.evtx_file.number_of_records > 0:
+            for i in range(0, len(self.evtx_file.records)):
+                dict_type = xmltodict.parse(self.evtx_file.records[i].get_xml_string())
+                json_type = json.dumps(dict_type)
+                dict2_type = json.loads(json_type)
+                try:
+                    if dict2_type['Event']['EventData'] is not None:
+                        log_obj = dict()
+                        log_obj["number"] = i
+                        log_obj["string"] = dict2_type['Event']['EventData']['Data']
+                        json_list.append(log_obj)
+                except:
+                    pass
+                print(log_obj)
+        return json_list
+
+    def __make_json(self):
+        json_list = []
+        for i in range(0, len(self.evtx_file.records)):
+            log_obj = dict()
+            log_obj["number"] = i
+            log_obj["eventID"] = self.evtx_file.records[i].get_event_identifier()
+            log_obj["create Time"] = str(self.evtx_file.records[i].get_creation_time())
+            log_obj["level"] = self.evtx_file.records[i].get_event_level()
+            log_obj["source"] = self.evtx_file.records[i].get_source_name()
+            log_obj["computer Info"] = self.evtx_file.records[i].get_computer_name()
+            log_obj["SID"] = self.evtx_file.records[i].get_user_security_identifier()
+            json_list.append(log_obj)
+        return json_list
+
+    def eventid(self, num):
+        for i in range(0, len(self.evtx_json)):
+            if self.evtx_json[i]['eventID'] == num:
+                self.result.append(self.evtx_json[i])
+                print(self.evtx_json[i])
+        return self.result
+
+
+    def level(self, num):
+        for i in range(0, len(self.evtx_json)):
+            if self.evtx_json[i]['level'] == num:
+                print(self.evtx_json[i])
+                self.result.append(self.evtx_json[i])
+        return self.result
+
+    def xml_with_num(self, num):
+        print(self.evtx_file.records[num].get_xml_string())
+        return self.evtx_file.records[num].get_xml_string()
+
+
+class Favorite:
+    def __init__(self, json):
+        self.evtx_json = json
+        self.accountType = AccountType(self.evtx_json)
+
+    def logon(self): #detect valid logon record
+        EvtxAnalysis.eventid(self, 4624)
+
+    def remote(self): #detect remote logon record
+        EvtxAnalysis.eventid(self, 540)
+        EvtxAnalysis.eventid(self, 4776)
+
+    def malfind(self): #detect malfind record
+        EvtxAnalysis.eventid(self, 4776)
+
+    def authority(self): #detect time of elevation of authority
+        print('user')
+
+
+class AccountType: #filtering based on logon type
+    def __init__(self, evtx_json):
+        self.evtx_json = evtx_json
+
+    def change_pwd(self):
+        EvtxAnalysis.eventid(self, 4723)
+
+    def delete_account(self):
+        EvtxAnalysis.eventid(self, 4726)
+
+    def verify_account(self):
+        EvtxAnalysis.eventid(self, 4720)
+
+
+class LinuxLogAnalysis:
+    def __init__(self, file):
+        self.file = file
