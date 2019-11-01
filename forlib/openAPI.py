e743b41df6bb2f4b32af60583b8ff9f481274b25
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-02 00:23:24 +0900

    Update openAPI.py

--- forlib/openAPI.py
+++ forlib/openAPI.py
@@ -64,7 +64,7 @@ def file_open(path):
     elif extension == 'Cache':
         file = cache_open(path)
         return thumbnail_analysis.Thumbnail_analysis_windows(file)
-    elif extension == 'lnk':
+    elif extension == 'MS Windows shortcut':
         file = lnk_open(path)
         return lnk_analysis.LnkAnalysis(file)
     # elif extension == 'PE32+ executable (console) x86-64':
