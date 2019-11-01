d6de4edea15f3e4be928194d825e57f031ce130e
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-02 02:53:27 +0900

    Update recycle_analysis.py

--- forlib/processing/recycle_analysis.py
+++ forlib/processing/recycle_analysis.py
@@ -22,17 +22,17 @@ class RecycleAnalysis:
         file_size = struct.unpack('<i', self.file.read(4))[0] + struct.unpack('<i', self.file.read(4))[0]
         print('File Size: '+str(file_size))
 
-        def r_time():
-            self.file.seek(16)
-            filedatetime = struct.unpack_from('<q', self.file.read(8))[0]
-            filedatetime = '%016x' %filedatetime
-            filedatetime = int(filedatetime,16)/10.
-            filedatetime =  datetime(1601, 1, 1) + timedelta(microseconds=filedatetime)+timedelta(hours=9)  
-            print('File Time: ' + str(filedatetime) + '+ UTC-9')
+    def r_time():
+        self.file.seek(16)
+        filedatetime = struct.unpack_from('<q', self.file.read(8))[0]
+        filedatetime = '%016x' %filedatetime
+        filedatetime = int(filedatetime,16)/10.
+        filedatetime =  datetime(1601, 1, 1) + timedelta(microseconds=filedatetime)+timedelta(hours=9)  
+        print('File Time: ' + str(filedatetime) + '+ UTC-9')
 
 
-        def r_path():
-            self.file.seek(24)
-            path = str(self.file.read(), 'cp1252')
-            path = path.replace('\x00','').encode('', 'ignore').decode('cp949', 'ignore')
-            print('File Path: ' + path)
+    def r_path():
+        self.file.seek(24)
+        path = str(self.file.read(), 'cp1252')
+        path = path.replace('\x00','').encode('', 'ignore').decode('cp949', 'ignore')
+        print('File Path: ' + path)
