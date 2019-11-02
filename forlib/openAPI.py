d5d0d3cc78230002093a54cab67e380992b83d5e
maplejh <54804674+maplejh@users.noreply.github.com>
2019-11-02 20:03:39 +0900

    Update openAPI.py

--- forlib/openAPI.py
+++ forlib/openAPI.py
@@ -5,6 +5,7 @@ import zipfile
 import sqlite3
 import PyPDF2
 import codecs
+import pyesedb
 from PIL import Image
 from Registry import Registry
 from os import listdir
@@ -168,7 +169,7 @@ def firefox_open(path):
     
     
 def ie_edge_open(path):
-    return path
+    return pyesedb.open(path, 'rb')
 
 
 def ole_open(path):
