376a3b72de44783836dfe112af74b05a76d4430b
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-02 18:04:47 +0900

    Update lnk_analysis.py

--- forlib/processing/lnk_analysis.py
+++ forlib/processing/lnk_analysis.py
@@ -1,11 +1,12 @@
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
 import struct
 from bitstring import BitArray
 import binascii
 
+
 class LnkAnalysis:
 
-    def __init__(self,file):
+    def __init__(self, file):
         self.file = file
         self.lnk_flag = []
         self.locbase_path_uni
@@ -15,9 +16,9 @@ class LnkAnalysis:
         self.extra_off
         self.extra_data
         self.linkinfo_flag
-        
-    def link_flag(self, flags_to_parse):   
-        flags = { 0: "HasLinkTargetIDList",
+
+    def link_flag(self, flags_to_parse):
+        flags = {0: "HasLinkTargetIDList",
                  1: "HasLinkInfo",
                  2: "HasName",
                  3: "HasRelativePath",
@@ -48,15 +49,15 @@ class LnkAnalysis:
 
         for count, items in enumerate(flags_to_parse):
             if int(items) == 1:
-                print ('Link flag : '+format(flags[count]))
+                print('Link flag : ' + format(flags[count]))
                 self.lnk_flag.append(format(flags[count]))
             else:
                 continue
 
         return self.lnk_flag
-        
-    def lnk_attrib(self, attrib_to_parse):    
-        attrib = { 0: "FILE_ATTRIBUTE_READONLY",
+
+    def lnk_attrib(self, attrib_to_parse):
+        attrib = {0: "FILE_ATTRIBUTE_READONLY",
                   1: "FILE_ATTRIBUTE_HIDDEN",
                   2: "FILE_ATTRIBUTE_SYSTEM",
                   3: "Reserved1",
@@ -70,17 +71,17 @@ class LnkAnalysis:
                   11: "FILE_ATTRIBUTE_COMPRESSED",
                   12: "FILE_ATTRIBUTE_OFFLINE",
                   13: "FILE_ATTRIBUTE_NOT_CONTENT_INDEXED",
-                  14: "FILE_ATTRIBUTE_ENCRYPTED" 
+                  14: "FILE_ATTRIBUTE_ENCRYPTED"
                   }
 
         lnk_attributes = []
         for count, items in enumerate(attrib_to_parse):
             if int(items) == 1:
-                print ('attrib: '+format(attrib[count]))
+                print('attrib: ' + format(attrib[count]))
                 lnk_attributes.append(format(attrib[count]))
             else:
                 continue
-                          
+
         return lnk_attributes
 
     def drive_type_list(self, drive):
@@ -92,40 +93,34 @@ class LnkAnalysis:
             4: 'DRIVE_REMOTE',
             5: 'DRIVE_CDROM',
             6: 'DRIVE_RAMDISK'
-            }
-    
+        }
+
         for count, items in enumerate(drive):
             if int(items) == 1:
-                print ('drive_type: '+format(drive_type[drive]))
+                print('drive_type: ' + format(drive_type[drive]))
             else:
                 continue
 
     def convert_time(time):
-        time = '%016x' %time
-        time = int(time,16)/10.
-        time =  datetime(1601, 1, 1) + timedelta(microseconds=time)+timedelta(hours=9)  
+        time = '%016x' % time
+        time = int(time, 16) / 10.
+        time = datetime(1601, 1, 1) + timedelta(microseconds=time) + timedelta(hours=9)
         return time
 
-## has link target id list has link info -- linkinfo flags
+    ## has link target id list has link info -- linkinfo flags
     def link_flags(self):
-        self.file
-        
         self.file.seek(20)
         flags = struct.unpack('<i', self.file.read(4))
         file_flags = BitArray(hex(flags[0]))
         link_flag(file_flags.bin)
-    
+
     def file_attribute(self):
-        self.file
-        
         self.file.seek(24)
         attributes = struct.unpack('<i', self.file.read(4))
         flag_atributes = BitArray(hex(attributes[0]))
         lnk_attrib(flag_atributes.bin)
 
     def creation_time(self):
-        self.file
-        
         self.file.seek(28)
         c_time = self.file.read(8)
         c_time = struct.unpack('<q', c_time)
@@ -133,8 +128,6 @@ class LnkAnalysis:
         print('Creation Time: ' + c_time)
 
     def access_time(self):
-        self.file
-    
         self.file.seek(36)
         a_time = self.file.read(8)
         a_time = struct.unpack_from('<q', a_time)[0]
@@ -142,8 +135,6 @@ class LnkAnalysis:
         print('Access Time: ' + a_time)
 
     def write_time(self):
-        self.file
-        
         self.file.seek(44)
         w_time = self.file.read(8)
         w_time = struct.unpack('<q', w_time)[0]
@@ -151,62 +142,47 @@ class LnkAnalysis:
         print('Write Time: ' + w_time)
 
     def file_size(self):
-        self.file
-        
         self.file.seek(52)
         file_size = struct.unpack('<l', self.file.read(4))[0]
-        print('Targrt File Size : '+str(file_size)+'bytes')
+        print('Targrt File Size : ' + str(file_size) + 'bytes')
 
     def iconindex(self):
-        self.file
-        
         self.file.seek(56)
         iconindex = struct.unpack('<l', self.file.read(4))[0]
-        print('Iconindex : '+ str(iconindex))
-
+        print('Iconindex : ' + str(iconindex))
 
     def show_command(self):
-        self.file
-        
         self.file.seek(60)
         showcomand = struct.unpack('<i', self.file.read(4))[0]
         showcomand = hex(showcomand)
         if showcomand == hex(0x1):
-            print ('SW_SHOWNORMAL')
+            print('SW_SHOWNORMAL')
         elif showcomand == hex(0x3):
-            print ('SW_SHOWMAXIMIZED') 
+            print('SW_SHOWMAXIMIZED')
         elif showcomand == hex(0x7):
-            print ('SW_SHOWMINNOACTIVE') 
+            print('SW_SHOWMINNOACTIVE')
         else:
-            print ('SW_SHOWNORMAL(default)')
-        
-############################################
+            print('SW_SHOWNORMAL(default)')
+
+    ############################################
 
     def linkinfo_off(self):
-        self.file
-        self.start_off
-        self.info_size
-        self.lnk_flag
-        self.locbase_path_uni
-        self.info_flag
-        self.linkinfo_flag
-    
         link_flags()
-    
+
         if 'HasLinkInfo' not in self.lnk_flag:
             self.linkinfo_flag = None
         else:
             self.linkinfo_flag = 'True'
-        
+
         if 'HasLinkTargetIDList' not in self.lnk_flag:
             self.start_off = 76
-        else:    
+        else:
             self.file.seek(76)
             items_hex = self.file.read(2)
             b = (b'\x00\x00')
-            items_hex  = items_hex + b
+            items_hex = items_hex + b
             idlistsize = struct.unpack('<i', items_hex)[0]
-            self.start_off = 78+idlistsize
+            self.start_off = 78 + idlistsize
 
         self.file.seek(self.start_off)
         self.info_size = struct.unpack('<i', self.file.read(4))[0]
@@ -214,7 +190,7 @@ class LnkAnalysis:
         if info_header_size == '\x00\x00\x00\x1C':
             info_option = 'False'
         else:
-            info_option = 'True'              
+            info_option = 'True'
         self.info_flag = self.file.read(4)
         if self.info_flag == '\x00\x00\x00\x01':
             self.info_flag = 'A'
@@ -222,30 +198,25 @@ class LnkAnalysis:
             locbase_path = 'True'
             net = None
             if info_option == 'True':
-                self.locbase_path_uni ='True'
+                self.locbase_path_uni = 'True'
             else:
-                self.locbase_path_uni = None 
+                self.locbase_path_uni = None
         else:
             self.info_flag = 'B'
             volume = None
             locbase_path = None
             net = 'True'
             self.locbase_path_uni = None
-    
+
     def volume(self):
-        self.file
-        self.info_flag
-        self.start_off
-        self.linkinfo_flag
-                          
         linkinfo_off()
-    
+
         if self.linkinfo_flag != 'True':
             print('this file does not have link info')
             return 0
         elif self.info_flag != 'A':
             return print('do not have volumeid')
-                          
+
         vol_off = self.start_off + 12
         self.file.seek(vol_off)
         volumeid_off = struct.unpack('<i', self.file.read(4))[0]
@@ -253,13 +224,13 @@ class LnkAnalysis:
         self.file.seek(volumeid_off)
 
         vol_size = struct.unpack('<i', self.file.read(4))[0]
-    
+
         drive_type = struct.unpack('<i', self.file.read(4))[0]
         drive_type_list(drive_type)
 
         driveserialnumber = struct.unpack('<l', self.file.read(4))[0]
-        print('Driveserialnumber: '+str(driveserialnumber))
-   
+        print('Driveserialnumber: ' + str(driveserialnumber))
+
         volumelable_off = self.file.read(4)
         if volumelable_off != '0x00000014':
             volumelable_off = struct.unpack('<l', volumelable_off)[0]
@@ -276,12 +247,6 @@ class LnkAnalysis:
         print('Volumelable: ' + volumelable)
 
     def localbase_path(self):
-        self.file
-        self.info_flag
-        self.start_off
-        self.locbase_path_uni
-        self.linkinfo_flag
-    
         linkinfo_off()
 
         if self.linkinfo_flag != 'True':
@@ -298,7 +263,7 @@ class LnkAnalysis:
         self.locbase_path_uni = locbase_path_off_uni + self.start_off
         self.file.seek(self.locbase_path_uni)
         self.locbase_path_uni = self.file.read(100)
-        self.locbase_path_uni = self.locbase_path_uni.decode('utf-8','ignore')
+        self.locbase_path_uni = self.locbase_path_uni.decode('utf-8', 'ignore')
         self.locbase_path_uni = []
         for i in self.locbase_path_uni.split('\x00\x00'):
             self.locbase_path_uni.append(i)
@@ -308,7 +273,7 @@ class LnkAnalysis:
         self.file.seek(locbasepath_off)
         locbasepath_off = struct.unpack('<l', self.file.read(4))[0]
         locbasepath_off = locbasepath_off + self.start_off
-                          
+
         self.file.seek(locbasepath_off)
         locbasepath = self.file.read(100)
         locbasepath = locbasepath.decode('utf-8', 'ignore')
@@ -317,58 +282,51 @@ class LnkAnalysis:
             locbasepath.append(i)
         print('Localbasepath: ' + locbasepath[0])
 
-############################################
-    
+    ############################################
+
     def extradata(self):
-        self.file
-        self.start_off
-        self.info_size
-        self.lnk_flag
-        self.extra_off
-        self.extra_data
-    
         linkinfo_off()
-    
+
         string_off = self.start_off + self.info_size
-    
+
         if 'HasName' in self.lnk_flag:
             self.file.seek(string_off)
             string_size = self.file.read(2)
             b = (b'\x00\x00')
-            string_size  = string_size + b
+            string_size = string_size + b
             string_size = struct.unpack('<i', string_size)[0]
             string_off = string_size + string_off + 2
         elif 'HasRelativePath' in self.lnk_flag:
             self.file.seek(string_off)
             string_size = self.file.read(2)
             b = (b'\x00\x00')
-            string_size  = string_size + b
+            string_size = string_size + b
             string_size = struct.unpack('<i', string_size)[0]
             string_off = string_size + string_off + 2
-##        relative_path = str(file.read(string_size))
-##        relative_path = relative_path.replace('\x00','').encode('utf-8', 'ignore').decode('utf-8')
+        ##        relative_path = str(file.read(string_size))
+        ##        relative_path = relative_path.replace('\x00','').encode('utf-8', 'ignore').decode('utf-8')
         elif 'HasWorkingDir' in self.lnk_flag:
             self.file.seek(string_off)
             string_size = self.file.read(2)
             b = (b'\x00\x00')
-            string_size  = string_size + b
+            string_size = string_size + b
             string_size = struct.unpack('<i', string_size)[0]
             string_off = string_size + string_off + 2
         elif 'HasArguments' in self.lnk_flag:
             self.file.seek(string_off)
             string_size = self.file.read(2)
             b = (b'\x00\x00')
-            string_size  = string_size + b
+            string_size = string_size + b
             string_size = struct.unpack('<i', string_size)[0]
             string_off = string_size + string_off + 2
         elif 'HasIconLocation' in self.lnk_flag:
             self.file.seek(string_off)
             string_size = self.file.read(2)
             b = (b'\x00\x00')
-            string_size  = string_size + b
+            string_size = string_size + b
             string_size = struct.unpack('<i', string_size)[0]
             string_off = string_size + string_off + 2
-        
+
         self.extra_off = string_off
         block_signature = self.extra_off + 4
         self.file.seek(block_signature)
@@ -379,41 +337,30 @@ class LnkAnalysis:
             self.extra_data = None
 
     def netbios(self):
-        self.file
-        self.extra_data
-        self.extra_off
-        
         extradata()
-        
+
         if self.extra_data != 'True':
             return print('not have extra data')
         netbios = self.extra_off + 16
         self.file.seek(netbios)
         netbios = str(self.file.read(16))
-        netbios = netbios.replace('\x00','').encode('utf-8', 'ignore').decode('utf-8')
+        netbios = netbios.replace('\x00', '').encode('utf-8', 'ignore').decode('utf-8')
         print('NetBios: ' + str(netbios))
 
     def machine_id(self):
-        self.file
-        self.extra_data
-        self.extra_off
-        
         extradata()
-        
+
         if self.extra_data != 'True':
             return print('not have extra data')
         droid = self.extra_off + 32
         self.file.seek(droid)
         droid = str(self.file.read(32))
-        droid = droid.replace('\x00','').encode('utf-8', 'ignore').decode('utf-8')
+        droid = droid.replace('\x00', '').encode('utf-8', 'ignore').decode('utf-8')
         print('Droid' + str(droid))
         droidbirth = str(self.file.read(32))
-        droidbirth = droidbirth.replace('\x00','').encode('utf-8', 'ignore').decode('utf-8')
+        droidbirth = droidbirth.replace('\x00', '').encode('utf-8', 'ignore').decode('utf-8')
         print('DroidBirth' + str(droidbirth))
-    
-        
-    
-    
+
 ##def favorite():
 ##    timestamp
 ##    volume
