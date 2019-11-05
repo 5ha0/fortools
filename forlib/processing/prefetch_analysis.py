7c6887245712730a542a6fafabaf396b8fdcc9c5
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-06 00:10:43 +0900

    Update prefetch_analysis.py

--- forlib/processing/prefetch_analysis.py
+++ forlib/processing/prefetch_analysis.py
@@ -10,7 +10,7 @@ class PrefetchAnalysis:
     def __init__(self,file):
         self.file = file
  
-    def pf_file_name(self):
+    def file_name(self):
         self.file.seek(16)
         file_name = self.file.read(58)
         file_name = file_name.decode('utf16', 'ignore')
@@ -18,7 +18,7 @@ class PrefetchAnalysis:
         print('Executable File Name: ' + file_name)
         return file_name
         
-    def pf_last_run_time(self):
+    def last_run_time(self):
         self.file.seek(128)
         time = struct.unpack_from("<Q", self.file.read(8))[0]
         time = '%016x' %time
@@ -27,29 +27,32 @@ class PrefetchAnalysis:
         print("File Last Run Time: " + str(last_run_time) +' UTC+9:00')
         return last_run_time
 
-    def pf_create_time(self, path):
+    def create_time(self, path):
         time = datetime.fromtimestamp(os.path.getctime(path))
         print ('File Create Time: '+str(time) +' UTC+9:00')
         return time
     
-    def pf_write_time(self, path):
+    def write_time(self, path):
         time = datetime.fromtimestamp(os.path.getmtime(path))
-        print ('File Create Time: '+ str(time) +' UTC+9:00')
+        print ('File Write Time: '+ str(time) +' UTC+9:00')
         return time
     
-    def pf_num_launch(self):
+    def num_launch(self):
         self.file.seek(0)
         version = struct.unpack_from('<I', self.file.read(4))[0]
         
         if version == 23:
             self.file.seek(152)
-            print('File Run Count:'+ str(struct.unpack_from('<I', self.file.read(4))[0]))
+            num_launch = struct.unpack_from('<I', self.file.read(4))[0]
         elif version ==30:
             self.file.seek(208)
-            print('File Run Count:'+ str(struct.unpack_from('<I', self.file.read(4))[0]))
+            num_launch = struct.unpack_from('<I', self.file.read(4))[0]
+            
+        print('File Run Count:'+ str(num_launch))
+        
         return num_launch
     
-    def pf_file_list(self):
+    def file_list(self):
         self.file.seek(100)
         file_list_offset=struct.unpack_from('<I', self.file.read(4))[0]
         file_list_size=struct.unpack_from('<I', self.file.read(4))[0]
@@ -70,5 +73,57 @@ class PrefetchAnalysis:
             print(json.dumps(pf_obj))
         return resource
     
-##class favorite:
-##    def time_stamp:
+class Favorite:
+    
+    def __init__(self, file):
+        self.file = file
+        
+    def time_stamp(self):
+        l_time = struct.unpack_from("<Q", self.file.read(8))[0]
+        l_time = '%016x' %l_time
+        l_time = int(l_time,16)/10.
+        l_time =  datetime(1601, 1, 1) + timedelta(microseconds=l_time)+timedelta(hours=9)  
+        
+        c_time = datetime.fromtimestamp(os.path.getctime(path))
+        
+        w_time = datetime.fromtimestamp(os.path.getmtime(path))
+        
+        print("File Last Run Time: " + str(last_run_time) +' UTC+9:00')
+        print ('File Create Time: '+str(c_time) +' UTC+9:00')
+        print ('File Write Time: '+ str(w_time) +' UTC+9:00')
+        
+        return l_time, c_time, w_time
+    
+    def file_list_whit_num_launch(self):
+        self.file.seek(0)
+        version = struct.unpack_from('<I', self.file.read(4))[0]
+        
+        if version == 23:
+            self.file.seek(152)
+            num_launch = struct.unpack_from('<I', self.file.read(4))[0]
+        elif version ==30:
+            self.file.seek(208)
+            num_launch = struct.unpack_from('<I', self.file.read(4))[0]
+            
+        self.file.seek(100)
+        file_list_offset=struct.unpack_from('<I', self.file.read(4))[0]
+        file_list_size=struct.unpack_from('<I', self.file.read(4))[0]
+        resource = []
+        self.file.seek(file_list_offset)
+        filenames = self.file.read(file_list_size)
+        filenames = filenames.decode('cp1252')
+        for i in filenames.split('\x00\x00'):
+            resource.append(i.replace('\x00',''))
+        
+        print('File Run Count:'+ str(num_launch))
+        
+        count = 0
+        for i in resource:
+            count += 1
+            pf_obj = {
+                "Num" : count,
+                "Ref_file" : i
+            }
+            print(json.dumps(pf_obj))
+        return num_launch, resource
+        
