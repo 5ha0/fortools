8ffc0350fa2d857ee418978cb2f3875ad875f44b
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-05 22:56:36 +0900

    Update recycle_analysis.py

--- forlib/processing/recycle_analysis.py
+++ forlib/processing/recycle_analysis.py
@@ -11,18 +11,18 @@ class RecycleAnalysis:
     def __init__(self, file):
         self.file = file
 
-    def r_header():
+    def r_header(self):
         self.file.seek(0)
         fileheader = self.file.read(8)
         fileheader = binascii.hexlify(fileheader).decode('ascii')
         print('File Header: ' + fileheader)
 
-    def r_size():
+    def r_size(self):
         self.file.seek(8)
         file_size = struct.unpack('<i', self.file.read(4))[0] + struct.unpack('<i', self.file.read(4))[0]
         print('File Size: '+str(file_size))
 
-    def r_time():
+    def r_time(self):
         self.file.seek(16)
         filedatetime = struct.unpack_from('<q', self.file.read(8))[0]
         filedatetime = '%016x' %filedatetime
@@ -31,7 +31,7 @@ class RecycleAnalysis:
         print('File Time: ' + str(filedatetime) + '+ UTC-9')
 
 
-    def r_path():
+    def r_path(self):
         self.file.seek(24)
         path = str(self.file.read(), 'cp1252')
         path = path.replace('\x00','').encode('', 'ignore').decode('cp949', 'ignore')
