1c14057c218367872c48e94a9a1778c863474391
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-08 06:00:53 +0900

    Update recycle_analysis.py

--- forlib/processing/recycle_analysis.py
+++ forlib/processing/recycle_analysis.py
@@ -7,28 +7,41 @@ class RecycleAnalysis:
     def __init__(self, file):
         self.file = file
 
-    def r_header(self):
+
+    def header(self):
         self.file.seek(0)
         fileheader = self.file.read(8)
         fileheader = binascii.hexlify(fileheader).decode('ascii')
         print('File Header: ' + fileheader)
 
-    def r_size(self):
+    def size(self):
         self.file.seek(8)
         file_size = struct.unpack('<i', self.file.read(4))[0] + struct.unpack('<i', self.file.read(4))[0]
-        print('File Size: '+str(file_size))
+        print('Original File Size: '+str(file_size))
 
-    def r_time(self):
+    def time(self):
         self.file.seek(16)
         filedatetime = struct.unpack_from('<q', self.file.read(8))[0]
         filedatetime = '%016x' %filedatetime
         filedatetime = int(filedatetime,16)/10.
-        filedatetime =  datetime(1601, 1, 1) + timedelta(microseconds=filedatetime)+timedelta(hours=9)  
-        print('File Time: ' + str(filedatetime) + '+ UTC+9:00')
+        filedatetime = datetime(1601, 1, 1) + timedelta(microseconds=filedatetime)+timedelta(hours=9)
+        print('File Deleted Time: ' + str(filedatetime) + '+ UTC+9:00')
 
 
-    def r_path(self):
+    def path(self):
         self.file.seek(24)
         path = str(self.file.read(), 'cp1252')
-        path = path.replace('\x00','').encode('', 'ignore').decode('cp949', 'ignore')
-        print('File Path: ' + path)
+        path = path.replace('\x00', '').encode('', 'ignore').decode('cp949', 'ignore')
+        print('Original File Path: ' + path)
+
+    def show_all_info(self):
+        info_list = []
+        info = dict()
+        info["file header"] = str(self.header())
+        info["original file size"] = str(self.size())
+        info["file deleted time"] = str(self.time())
+        info["original file path"] = str(self.path())
+
+        print(info)
+        info_list.append(info)
+        return info_list
