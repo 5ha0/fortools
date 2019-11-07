ab9b56243bcdd9280f38e65c8e34e2c7894fa90e
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-08 06:03:15 +0900

    Update iconcache_analysis.py

--- forlib/processing/iconcache_analysis.py
+++ forlib/processing/iconcache_analysis.py
@@ -6,6 +6,7 @@ class IconcacheAnalysis:
         self.file = file
         self.size = None
         self.signature = None
+        self.path_num = None
         
     def file_version(self):
         self.file.seek(12)
@@ -27,13 +28,13 @@ class IconcacheAnalysis:
         self.size = struct.unpack_from('<I', self.file.read(4))[0]
         
         self.file.seek(self.size)
-        path_num = struct.unpack_from('<i', self.file.read(4))[0]
+        self.path_num = struct.unpack_from('<i', self.file.read(4))[0]
         
         self.signature = self.file.read(2)
         
         path_length = self.file.read(2)
         b = (b'\x00\x00')
-        path_length  = path_length + b
+        path_length = path_length + b
         path_length = struct.unpack('<i', path_length)[0]
         
         sig_chek_list = ['\x02' '\x22' '\x42']
@@ -41,16 +42,17 @@ class IconcacheAnalysis:
             path_length = path_length
         else:
             path_length = path_length * 2
-        
+
+        print('path information in section 1')
         i = 0
-        while i != path_num:
+        while i != self.path_num:
             filepaths = self.file.read(path_length)
             filepaths = filepaths.decode('utf16', 'ignore')
             filepaths = filepaths.replace('\x00','')
             print(str(filepaths)+'\n')
             i += 1
 
-        self.size = self.size + path_length * path_num + 12
+        self.size = self.size + path_length * self.path_num + 12
         
         return filepaths
 
@@ -59,24 +61,25 @@ class IconcacheAnalysis:
         self.file.seek(self.size)
         path_length = self.file.read(2)
         b = (b'\x00\x00')
-        path_length  = path_length + b
-        path_length = struct.unpack('<i', self.path_length)[0]
+        path_length = path_length + b
+        path_length = struct.unpack('<i', path_length)[0]
         
         sig_chek_list = ['\x02' '\x22' '\x42']
         if self.signature == sig_chek_list:
             path_length = path_length
         else:
             path_length = path_length * 2
-        
+
+        print('path information in section 2')
         i = 0
-        while i != path_num:
+        while i != self.path_num:
             filepaths = self.file.read(path_length)
             filepaths = filepaths.decode('utf-16', 'ignore')
             filepaths = filepaths.replace('\x00','')
             print(str(filepaths)+'\n')
             i += 1
 
-        self.size = self.size + path_length * path_num + 14
+        self.size = self.size + path_length * self.path_num + 14
         
         return filepaths
     
@@ -85,7 +88,7 @@ class IconcacheAnalysis:
         self.file.seek(self.size)
         path_length = self.file.read(2)
         b = (b'\x00\x00')
-        path_length  = path_length + b
+        path_length = path_length + b
         path_length = struct.unpack('<i', path_length)[0]
         
         sig_chek_list = ['\x02' '\x22' '\x42']
@@ -93,13 +96,27 @@ class IconcacheAnalysis:
             path_length = path_length
         else:
             path_length = path_length * 2
-        
+
+        print('path information in section 3')
         i = 0
-        while i != path_num:
-            filepaths = self.file.read(read_num)
+        while i != self.path_num:
+            filepaths = self.file.read(path_length)
             filepaths = filepaths.decode('utf-16', 'ignore')
             filepaths = filepaths.replace('\x00','')
             print(str(filepaths)+'\n')
             i += 1
             
         return filepaths
+
+
+    def show_all_info(self):
+        info_list = []
+        info = dict()
+        info["file version"] = str(self.file_version())
+        info["path information_section one"] = str(self.section_one())
+        info["path information_section two"] = str(self.section_two())
+        info["path information_section three"] = str(self.section_three())
+
+        print(info)
+        info_list.append(info)
+        return info_list
