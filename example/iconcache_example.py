1e8739bda2366df82fb6cf50486bdeb56896490f
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-08 05:59:15 +0900

    Update iconcache_example.py

--- example/iconcache_example.py
+++ example/iconcache_example.py
@@ -1,11 +1,15 @@
 from fortools import *
 
-file = Iconcache.file_open('path')
+file = Iconcache.file_open('C:\\Users\\graci\\AppData\\Local\\IconCache.db')
+# Or you ca use this
+# path = 'path'
+# file = file_open(path)
 
-# show information from IconCache.db
+# show information about IconCache.db
 version = file.file_version()
-list1 = file.section_one()
-list2 = file.section_two()
-list3 = file.section_three()
+path_information1 = file.section_one()
+path_information2 = file.section_two()
+path_information3 = file.section_three()
 
-# show information from IconCache_##.db
+# show information from show_all_info()
+result = file.show_all_info()
