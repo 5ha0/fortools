d9e8390d3afb81ccb776a468f93e393cf2a5b7aa
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-07 17:58:57 +0900

    Update prefetch_example.py

--- example/prefetch_example.py
+++ example/prefetch_example.py
@@ -1,12 +1,11 @@
 from fortools import *
 
-path = 'C:\\Windows\\Prefetch\\CHROME.EXE-5349D2D7.pf'
-file = Prefetch.file_open(path)
+file = Prefetch.file_open('path')
 
 # show information from prefetch file
 name = file.file_name()
 l_time = file.last_run_time()
-c_time = file.create_time(path)
-w_time = file.write_time(path)
+c_time = file.create_time()
+w_time = file.write_time()
 launch = file.num_launch()
 file_list = file.file_list()
