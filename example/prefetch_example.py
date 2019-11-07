e99e89f464475be495e11bfe3c4451222dad5e2a
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-08 05:59:33 +0900

    Update prefetch_example.py

--- example/prefetch_example.py
+++ example/prefetch_example.py
@@ -1,11 +1,17 @@
 from fortools import *
 
-file = Prefetch.file_open('path')
+file = Prefetch.file_open(r'C:\Windows\Prefetch\IEXPLORE.EXE-A033F7A2.pf')
+# Or you ca use this
+# path = 'path'
+# file = file_open(path)
 
-# show information from prefetch file
+# show information about prefetch file
 name = file.file_name()
 l_time = file.last_run_time()
 c_time = file.create_time()
 w_time = file.write_time()
 launch = file.num_launch()
+file_list = file.file_list()
+
+# show information from show_all_info()
 file_list = file.file_list()
