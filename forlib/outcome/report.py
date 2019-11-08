2f126f277cb52a64a5cf6d7e6bedfeba67646121
baeha <54804672+baeha@users.noreply.github.com>
2019-11-08 17:20:16 +0900

    Add error processing

--- forlib/outcome/report.py
+++ forlib/outcome/report.py
@@ -22,17 +22,28 @@ class DocxExport:
         table.style = 'Table Grid'
         table.rows[0].style = "borderColor:red;background-color:gray"
 
-        col_list = list(data[0].keys())
+        try:
+            col_list = list(data[0].keys())
+        except ValueError as e:
+            print("That was no valid character."+e)
 
         hdr_cells = table.rows[0].cells
         for i in range(0, len(data[0].keys())):
-            hdr_cells[i].text = str(col_list[i])
+            try:
+                hdr_cells[i].text = str(col_list[i])
+            except ValueError as e:
+                print("That was no valid character." + str(e))
+                hdr_cells[i].text = 'none'
 
         for i in range(0, len(data)):
-            row_cells = table.add_row().cells
-            result = list(data[i].values())
-            for j in range(0, len(data[0].keys())):
-                row_cells[j].text = str(result[j])
+                row_cells = table.add_row().cells
+                result = list(data[i].values())
+                for j in range(0, len(data[0].keys())):
+                    try:
+                        row_cells[j].text = str(result[j])
+                    except ValueError as e:
+                        print("That was no valid character." + str(e))
+                        row_cells[j].text = 'none'
 
     def insert_img(self, path):
         self.document.add_picture(path, width=Cm(15), height=Cm(15))
