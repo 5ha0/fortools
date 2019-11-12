6fd3d3bdfaa4c29ed8be84e9c4e466ccc1e1d645
baeha <baehayoung0214@gmail.com>
2019-11-13 03:02:22 +0900

    Add file size

--- forlib/processing/jump_analysis.py
+++ forlib/processing/jump_analysis.py
@@ -1,13 +1,14 @@
 from forlib.processing.lnk_analysis import convert_time
 import struct
+import re
 
 
 class JumplistAnalysis:
-    json_list = []
 
     def __init__(self, file):
         self.file = file
         self.streams = file.listdir(streams=True, storages=False)
+        self.json_list = []
         self.__stream()
 
     def __stream(self):
@@ -24,11 +25,14 @@ class JumplistAnalysis:
                     data_list["access time"] = str(convert_time(a_time[0]))
                     w_time = struct.unpack("<q", file_data[44:52])
                     data_list["write time"] = str(convert_time(w_time[0]))
-                    data_list["file size"] = struct.unpack("<l", file_data[52:56])[0]
+                    file_size = struct.unpack("<l", file_data[52:56])[0]
+                    data_list["file size"] = self.file.get_size(self.streams[i])
+                    data_list["target file size"] = file_size
                     try:
-                        data_list["path"] = self.__path_info(file_data)
-                    except:
-                        data_list["path"] = 'none'
+                        return_path = self.__path_info(file_data)
+                        data_list["path"] = return_path.decode('ascii').replace('\x00', '')
+                    except Exception as e:
+                        data_list["path"] = return_path.decode('ascii').replace('\x00', '')
                     self.json_list.append(data_list)
                 else:
                     self.destlist = file_data
@@ -38,10 +42,17 @@ class JumplistAnalysis:
     def __path_info(self, file_data):
         lnk_id_list_size = struct.unpack('<h', file_data[76:78])
         lnk_info_size = struct.unpack('<l', file_data[78+lnk_id_list_size[0]:82 + lnk_id_list_size[0]])
-        path_offset = struct.unpack('<l', file_data[94+lnk_id_list_size[0]:94+lnk_id_list_size[0]+4])
-        size = lnk_info_size[0] - path_offset[0]
-        local = file_data[78+lnk_id_list_size[0]+path_offset[0]:78+lnk_id_list_size[0]+path_offset[0] + size]
-        return local.decode('ascii')[:-2]
+        # lnk_volume_id_offset = struct.unpack("<l", file_data[90+lnk_id_list_size[0]:90+lnk_id_list_size[0]+4])
+        lnk_info_flag = struct.unpack("<L", file_data[86 + lnk_id_list_size[0]:86 + lnk_id_list_size[0] + 4])
+        path_offset = struct.unpack('<l', file_data[94 + lnk_id_list_size[0]:94 + lnk_id_list_size[0] + 4])
+        if lnk_info_flag[0] == 1:
+            size = lnk_info_size[0] - path_offset[0]
+            local = file_data[78+lnk_id_list_size[0]+path_offset[0]:78+lnk_id_list_size[0]+path_offset[0] + size]
+        elif lnk_info_flag[0] == 3:
+            net_offset = struct.unpack("<l", file_data[98+lnk_id_list_size[0]:102+lnk_id_list_size[0]])
+            size = net_offset[0] - path_offset[0]
+            local = file_data[78 + lnk_id_list_size[0] + path_offset[0]:78 + lnk_id_list_size[0] + path_offset[0] + size]
+        return local
 
     def access_count(self):
         cnt = struct.unpack("<l", self.destlist[148:152])
@@ -51,6 +62,13 @@ class JumplistAnalysis:
         time = struct.unpack("<q", self.destlist[132:140])
         return str(convert_time(time[0]))
 
+    def netbios(self):
+        netbios = self.destlist[104:120]
+        return netbios.decode('ascii')
+
     def show_info(self):
         for i in self.json_list:
             print(i)
+
+    def get_info(self):
+        return self.json_list
