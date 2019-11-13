936964ece9170f5a5d38a806c9246f1314d540c2
baeha <baehayoung0214@gmail.com>
2019-11-13 16:56:55 +0900

    Add convert time

---
+++ forlib/processing/convert_time.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timedelta
+
+
+def convert_time(time):
+    time = '%016x' % time
+    time = int(time, 16) / 10.
+    time = datetime(1601, 1, 1) + timedelta(microseconds=time) + timedelta(hours=9)
+    return time
