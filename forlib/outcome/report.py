3723690f3f043ca1496ba7c0f4678b5c7ec66a97
baeha <baehayoung0214@gmail.com>
2019-11-19 02:03:19 +0900

    Add md_Export

--- forlib/outcome/report.py
+++ forlib/outcome/report.py
@@ -2,13 +2,65 @@ import docx
 from datetime import datetime
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Cm
-# class MdExport:
+import os
+import errno
+
+
+class MdExport:
+    def __init__(self, name):
+        try:
+            if not (os.path.isdir('result')):
+                os.makedirs(os.path.join('result'))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                print("fail to create folder")
+                raise
+        file = open('./result/'+name+'.md', 'w')
+        self.file = file
+        self.file.write('# <center>Analysis Report\n\n')
+        self.file.write('<div style = "text-align: right"> '+str(datetime.now())+' </div>\n\n\n')
+
+    def add_text(self, contents):
+        self.file.write('\n'+contents+'\n\n')
+
+    def emphasis(self, contents):
+        self.file.write('\n\n**'+str(contents)+'**\n\n')
+
+    def make_table(self, data):
+        self.file.write('\n\n')
+        if len(data) == 0:
+            print("There is no data")
+            return -1
+        self.file.write('|')
+        for i in data[0].keys():
+            self.file.write('<center>'+i+'|')
+        self.file.write('\n')
+        for i in data[0].keys():
+            self.file.write('--------------------|')
+
+        self.file.write('\n')
+        for i in range(0, len(data)):
+            result = list(data[i].values())
+            for j in range(0, len(data[0].keys())):
+                self.file.write('<center>'+str(result[j])+'|')
+            self.file.write('\n')
+        self.file.write('\n\n')
+
+    def save(self):
+        self.file.close()
 
 
 class DocxExport:
     def __init__(self):
+        try:
+            if not (os.path.isdir('result')):
+                os.makedirs(os.path.join('result'))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                print("fail to create folder")
+                raise
         document = docx.Document()
-        heading = document.add_heading('분석 보고서', 0)
+        heading = document.add_heading('Analysis Report', 0)
         heading.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
         date_info = document.add_paragraph(str(datetime.now()))
         date_info.alignment = WD_PARAGRAPH_ALIGNMENT.RIGHT
@@ -17,7 +69,7 @@ class DocxExport:
 
         self.document = document
 
-    def make_table(self, data):
+    def add_table(self, data):
         if len(data) == 0:
             print("There is no data")
             return -1
@@ -37,12 +89,12 @@ class DocxExport:
             for j in range(0, len(data[0].keys())):
                 row_cells[j].text = str(result[j])
 
-    def insert_img(self, path):
+    def add_img(self, path):
         self.document.add_picture(path, width=Cm(15), height=Cm(15))
 
     def add_text(self, text):
         self.document.add_paragraph(str(text))
 
     def save(self, name):
-        self.document.save(name + '.docx')
+        self.document.save('./result/' + name + '.docx')
 
