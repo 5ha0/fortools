d84637e9704c49fd28bfb73a4d03137501dde35d
gut27 <gracious7272@gmail.com>
2019-11-22 21:00:11 +0900

    modify

--- example/recycle_example.py
+++ example/recycle_example.py
@@ -1,15 +1,16 @@
 from fortools import *
 
-file = Recycle.file_open('path')
+file = Recycle.file_open('D:\\CFReDS\\DiskImage\\$I30')
 # Or you ca use this
 # path = 'path'
 # file = file_open(path)
 
-# show information about $I recycle file
+# show each information about recycle $I file
+name = file.i_name()
 header = file.header()
 size = file.size()
 time = file.time()
 path = file.path()
 
-# show information from show_all_info()
+# show all information about recycle $I file
 result = file.show_all_info()
