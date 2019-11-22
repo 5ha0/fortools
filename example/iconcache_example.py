51c64693e756d8f787a4ff782f84b369bb7cf46c
gut27 <gracious7272@gmail.com>
2019-11-23 05:57:59 +0900

    modify

--- example/iconcache_example.py
+++ example/iconcache_example.py
@@ -1,16 +1,20 @@
 from fortools import *
 
-# file = Iconcache.file_open('C:\\Users\\graci\\AppData\\Local\\IconCache.db')
-file = Iconcache.file_open(r'C:\Users\graci\Desktop\IconCache\IconCache.db')
+file = Iconcache.file_open(r'C:\Users\graci\Desktop\cfreds_11\IconCache.db')
 # Or you ca use this
 # path = 'path'
 # file = file_open(path)
 
 # show information about IconCache.db
-version = file.file_version()
-path_information1 = file.section_one()
-path_information2 = file.section_two()
-path_information3 = file.section_three()
+information = file.all_info()
 
-# show information from show_all_info()
-doc = file.show_all_info()
+# Allows you to check whether the drive delete program is used.
+file.drive_delete_exe()
+
+# Allows you to find a specific extension file.
+extension = '.extension'
+file.extension_filter(extension)
+
+# if you want to look time stamp for filepath, use this
+filepath = r'enter the file path you want to check the time of'
+file.show_time(filepath)
