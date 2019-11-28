0369e27b9faec972b2085394094966d3b7eab476
wk1219 <sjms1215@naver.com>
2019-11-28 21:43:20 +0900

    update thumbcache_analysis

---
+++ example/iconcache_example.py
@@ -0,0 +1,20 @@
+from fortools import *
+
+file = Iconcache.file_open(r'path')
+# Or you ca use this
+# path = 'path'
+# file = file_open(path)
+
+# show information about IconCache.db
+information = file.all_info()
+
+# Allows you to check whether the drive delete program is used.
+file.drive_delete_exe()
+
+# Allows you to find a specific extension file.
+extension = '.extension'
+file.extension_filter(extension)
+
+# if you want to look time stamp for filepath, use this
+filepath = r'enter the file path you want to check the time of'
+file.show_time(filepath)
