28a65461a77303ea4c6ba90a41c7396d2f1e6ff4
gut27 <52551159+gut27@users.noreply.github.com>
2019-11-29 00:23:51 +0900

    Update recycle_example.py

--- example/recycle_example.py
+++ example/recycle_example.py
@@ -1,16 +1,24 @@
 from fortools import *
 
-file = Recycle.file_open('D:\\CFReDS\\DiskImage\\$I30')
-# Or you ca use this
+file = Recycle.file_open(r'path')
+# Or you can use this
 # path = 'path'
 # file = file_open(path)
 
-# show each information about recycle $I file
-name = file.i_name()
-header = file.header()
-size = file.size()
-time = file.time()
-path = file.path()
+# #Uncomment the comment you want to use.# #
 
-# show all information about recycle $I file
-result = file.show_all_info()
+# #1. It shows all the parsing provided by this library at once
+# information = file.show_all_info()
+# #How to make a report
+# docx = DocxExport()
+# docx.make_table(information)
+# docx.save('report_name')
+
+# #2. It handles all parsing provided by this library at once.
+# information = file.get_all_info()
+# # if you show 2, use print()
+# print(information)
+# #How to make a report
+# docx = DocxExport()
+# docx.make_table(information)
+# docx.save('report_name')
