a5b996c7c061311716a4fdfbbb5c8378873517cd
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-12-01 19:59:54 +0900

    Update disk_example.py

--- example/disk_example.py
+++ example/disk_example.py
@@ -1,20 +1,29 @@
 from fortools import Disk
 
-file = Disk.disk_open(r"..\dataset\disk\cfreds_2015_data_leakage_pc.E01")
+file = Disk.disk_open(r"D:\새 폴더 (3)\Adam Ferrante - Laptop-Deadbox\Horcrux\Horcrux.E01")
+'''
+Precautions
+1. You should check volume partition information before collecting files.
+2. You must enter the partition start sector you want to analyze.
+'''
 
-# # disk file info
+############ File Collect ###############
+# 1. Navigate File Locations in Disk Images (get_path (find_path - start is ., partition start sector))
+rel = file.get_path("./Windows/System32/config", 1126400)
+# # 2. Extract files based on the information you collect
+# extract_SOFTWARE = file.file_extract(1126400, "./Windows/System32/config/SOFTWARE", "extrct_SOFTWARE")
+# extract_SAM = file.file_extract(1126400, "./Windows/System32/config/SAM", "extract_SAM")
+# # 3. File system log extraction
+# file.fslog_extract()
+# 
+# ############ File Analysis ###############
+# # 1. Output basic metadata of the DD.
+# rel = file.dd_metadata()
+# # 2. Output basic metadata of the E01.
 # rel = file.e01_metadata()
-#
-# # partition info
+# # 3. Print volume partition information.
 # rel = file.volume_metadata()
-
-# get_path (find_path - start is ., partition start sector)
-# You can find partition start sector in volume_metadata
-
-rel = file.get_path(".", 2048)
-
-## extract filesystemlog
-#rel = file.fslog_extract()
 
 for i in range(len(rel)):
     print(rel[i])
+
