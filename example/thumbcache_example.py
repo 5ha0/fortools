6924677665b9f48a9cb17a372d2a357afb5f1719
wk1219 <sjms1215@naver.com>
2019-12-02 17:02:18 +0900

    update thumbcache_example

--- example/thumbcache_example.py
+++ example/thumbcache_example.py
@@ -1,11 +1,10 @@
 from fortools import *
 
 #input file path
-path = r'C:\Users\sjms1\AppData\Local\Microsoft\Windows\Explorer\thumbcache_32.db'
+path = r'..\thumbcache_32.db'
 
 # thumbcache_xx.db, iconcache_xx.db file open
 thumbnail = file_open(path)
-#thumbnail.thumb_print()
 
 ####### 1. It shows all the parsing provided by this library at once. #######
 # thumbnail.thumb_print()
@@ -13,16 +12,16 @@ thumbnail = file_open(path)
 ####### 2. It handles all parsing provided by this library at once. #######
 information = thumbnail.get_info()
 
-# thumbnail.dimension(32, 8)
+####### 3. It shows specific width, height file data by this library at once. #######
+# info = thumbnail.dimension(32, 8)
 
+####### 3.If You want to make a report, Use This. #######
+# docu = DocxExport()
+# docu.add_table(info) # [{key:value},{key:value}]
+# docu.save('example')
 
-
-
-# filter_info = custom_filter(['file_name', r'^.+[.]bmp$', 1], data)
+####### 5.If You want to specific search, Use This Filter. #######
+# filter_info = custom_filter(['file_name', r'^.+[.]bmp$', 1], information)
 # for i in filter_info:
 #    print(i)
 
-# docu = DocxExport()
-# print("Hi")
-# docu.make_table(data) # [{key:value},{key:value}]
-# docu.save('example')
