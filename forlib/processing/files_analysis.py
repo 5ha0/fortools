3fac99e17e032e7de728bc9975fcf90df3b90704
wk1219 <sjms1215@naver.com>
2019-12-02 21:10:07 +0900

    modify zipfile_analysis

--- forlib/processing/files_analysis.py
+++ forlib/processing/files_analysis.py
@@ -252,7 +252,7 @@ class ZIPAnalysis:
             file_obj['FileName'] = file_name
             file_obj['Comment'] = str(info.comment)
 
-            mod_time = datetime(*info.date_time
+            mod_time = datetime(*info.date_time)
                                 
             mtime = str(mod_time.astimezone()).split('+')[1].split(':')[0].split('0')[1]
             file_obj['Modified'] = str(mod_time) + " UTC+" + str(mtime)
