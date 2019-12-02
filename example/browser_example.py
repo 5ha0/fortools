c6c0c8a19776f19ead2bc2f6c9c6c0be83248eeb
maplejh <54804674+maplejh@users.noreply.github.com>
2019-12-02 22:08:13 +0900

    Update browser_example.py

--- example/browser_example.py
+++ example/browser_example.py
@@ -2,55 +2,77 @@ from fortools import *
 import time
 start_time = time.time()
 
-chrome_cookies = Browser.Chrome.Cookie.file_open(cookies_path)
-chrome_history = Browser.Chrome.History.file_open(history_path)
-chrome_download = Browser.Chrome.Download.file_open(download_path)
-chrome_cache=Browser.Chrome.Cache.file_open(cache_path)
+# cookies_path = r"C:\Users\JH\Desktop\cfreds\Google\Chrome\User Data\Default\Cookies"
+# history_path = r"C:\Users\JH\Desktop\cfreds\Google\Chrome\User Data\Default\History"
+# download_path=r"C:\Users\JH\Desktop\cfreds\Google\Chrome\User Data\Default\History"
+# cache_path=r"C:\Users\JH\Desktop\cfreds\Google\Chrome\User Data\Default\Cache"
+# 
+# chrome_cookies = Browser.Chrome.Cookie.file_open(cookies_path)
+# chrome_history = Browser.Chrome.History.file_open(history_path)
+# chrome_download = Browser.Chrome.Download.file_open(download_path)
+# chrome_cache=Browser.Chrome.Cache.file_open(cache_path)
+#
+# print(chrome_cookies.get_hash())
+# print(chrome_history.get_hash())
+# print(chrome_download.get_hash())
+#
+# print("history")
+# for i in chrome_history.get_info():
+#    print(i)
+# print("cookies")
+# for i in chrome_cookies.get_info():
+#     print(i)
+# print("download")
+# for i in chrome_download.get_info():
+#     print(i)
+# print("cache")
+# for i in chrome_cache.get_info():
+#     print(i)
+#
+# path=r"C:\Users\JH\Desktop\cfreds\Microsoft\Windows\WebCache\WebCacheV01.dat"
+#
+# IE_Edge_cookies = Browser.Ie_Edge.Cookie.file_open(path)
+# IE_Edge_history = Browser.Ie_Edge.History.file_open(path)
+# IE_Edge_download=Browser.Ie_Edge.Download.file_open(path)
+# IE_Edge_cache=Browser.Ie_Edge.Cache.file_open(path)
+#
+# print(IE_Edge_cookies.get_hash())
+# print(IE_Edge_history.get_hash())
+# print(IE_Edge_download.get_hash())
+# print(IE_Edge_cache.get_hash())
+#
+# print("history")
+# for i in IE_Edge_history.get_info():
+#     print(i)
+# print("cookie")
+# for i in IE_Edge_cookies.get_info():
+#     print(i)
+# print("download")
+# for i in IE_Edge_download.get_info():
+#      print(i)
+# print("cache")
+# for i in IE_Edge_cache.get_info():
+#      print(i)
 
-print("history")
-for i in chrome_history.get_info():
-   print(i)
-print("cookies")
-for i in chrome_cookies.get_info():
-    print(i)
-print("download")
-for i in chrome_download.get_info():
-    print(i)
-print("cache")
-for i in chrome_cache.get_info():
-    print(i)
+# #keyword_search
+# keyword =chrome_history.keyword_search("keyword")
+# for i in keyword:
+#     print(i)
+# keyword =IE_Edge_history.keyword_search("keyword")
+# for i in keyword:
+#     print(i)
 
+# #cnt_sort
+# cnt_list = IE_Edge_history.cnt_sort()
+# for i in cnt_list:
+#     print(i)
+# cnt_list = chrome_history.cnt_sort()
+# for i in cnt_list:
+#     print(i)
 
-#get hash
-print(chrome_cookies.get_hash())
-print(chrome_history.get_hash())
-print(chrome_download.get_hash())
-
-# path= path of WebCacheV01.dat
-
-IE_Edge_cookies = Browser.Ie_Edge.Cookie.file_open(path)
-IE_Edge_history = Browser.Ie_Edge.History.file_open(path)
-IE_Edge_download=Browser.Ie_Edge.Download.file_open(path)
-IE_Edge_cache=Browser.Ie_Edge.Cache.file_open(path)
-
-print("history")
-for i in IE_Edge_history.get_info():
-    print(i)
-print("cookie")
-for i in IE_Edge_cookies.get_info():
-    print(i)
-print("download")
-for i in IE_Edge_download.get_info():
-     print(i)
-print("cache")
-for i in IE_Edge_cache.get_info():
-     print(i)
-
-#get hash
-print(IE_Edge_cookies.get_hash())
-print(IE_Edge_history.get_hash())
-print(IE_Edge_download.get_hash())
-print(IE_Edge_cache.get_hash())
-
-
-print("---{}s seconds---".format(time.time()-start_time))
+# #make report
+# get_docx = DocxExport()
+# get_docx.add_table(IE_Edge_cookies.get_info())
+# get_docx.save("report_name")
+#
+# print("---{}s seconds---".format(time.time()-start_time))
