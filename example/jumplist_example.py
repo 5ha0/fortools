f66f4ae1e56c97e86b77e0b37da5fc97a5dfc0df
baeha <baehayoung0214@gmail.com>
2019-12-03 16:07:08 +0900

    Add new functions

--- example/jumplist_example.py
+++ example/jumplist_example.py
@@ -1,12 +1,44 @@
 from fortools import *
 
-file = JumpList.file_open('path')
+# example of AutomaticDestinations
+# jumplist(.AutomaticDestinations) open
+path = r'your path'
+jumplist = JumpList.file_open(path)
 
-# show information from destlist
-access_cnt = file.access_count()
-recent_time = file.recent_time()
-print("cnt: " + str(access_cnt))
-print("last access time: " + str(recent_time))
 
-# show information about jumplist lnk
-file.show_info()
+'''
+Uncomment the comment you want to use.
+'''
+
+# # ------------------------1. Get data list from destlist.---------------------------
+# # You need to input window_version 7 or 10 according your os
+# # information: entry number of JumpList, count of add, delete, re-open event, netbios, last access time, access count, path
+# summary = jumplist.get_summary(window_version)
+# for i in summary:
+# 	print(i)
+#
+# # ------------------------2. Get data list from destlist.---------------------------
+# # You need to input window_version 7 or 10 according your os
+# # information: MAC(new/birth), netbios, last access time, path, time(new/birth)
+# dest_list = jumplist.get_destlist_data(window_version)
+# for i in dest_list:
+#     print(i)
+#
+# # ------------------------3. Get information from streams.---------------------------
+# # You need to input window_version 7 or 10 according your os
+# # infomation: MAC(Modification, Access, Creation) time, file size, target file size, path, drive type, drive serial numbr, volume label
+# info_list = jumplist.get_info
+# for i in info_list:
+# 	print(i)
+#
+# # ------------------------4. Print information from streams.---------------------------
+# # You need to input window_version 7 or 10 according your os
+# jumplist.show_info()
+#
+# # ------------------------5. Get hash value of jumplist file.---------------------------
+# # You need to input window_version 7 or 10 according your os
+# jump_hash = jumplist.get_hash()
+# for i in jump_hash:
+# 	print(i)
+
+
