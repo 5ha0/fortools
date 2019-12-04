c269012519ebdd81060ea295ddc22e5783170a60
baeha <54804672+baeha@users.noreply.github.com>
2019-12-04 16:06:56 +0900

    Update visualize.py

--- forlib/outcome/visualize.py
+++ forlib/outcome/visualize.py
@@ -1,6 +1,6 @@
 import errno
 import os
-#from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 
 class PieChart:
@@ -10,6 +10,7 @@ class PieChart:
         self.__show()
 
     def __show(self):
+        plt.figure(3)
         plt.pie(self.data, labels=self.label)
         try:
             if not (os.path.isdir('result')):
@@ -22,14 +23,12 @@ class PieChart:
 
 
 class Timeline:
-    def __init__(self, data):
-        for i in range(0, len(data)):
-            artifact1 = data[i]
-            x = artifact1["time"]
-            y = artifact1["num"]
-            plt.plot(x, y)
-            plt.xlabel('Time')
-            plt.ylabel('Num')
+    def __init__(self, data, file_name):
+        keys = list(data.keys())
+        values = list(data.values())
+        plt.figure(2)
+        plt.plot(keys, values)
+        plt.xticks(rotation=90, fontsize=5)
         try:
             if not (os.path.isdir('result')):
                 os.makedirs(os.path.join('result'))
@@ -37,4 +36,26 @@ class Timeline:
             if e.errno != errno.EEXIST:
                 print("fail to create folder")
                 raise
-        plt.savefig('./result/time.png')
+        plt.savefig('./result/'+file_name+'.png')
+
+class BarChart:
+    def __init__(self, data, file_name):
+        self.data = data
+        self.file_name = file_name
+        self.__show()
+
+    def __show(self):
+        x = list(self.data.keys())
+        y = list(self.data.values())
+        plt.figure(figsize=(8, 7))
+        plt.bar(x, y, width=0.5)
+        plt.xticks(rotation=90, fontsize=8)
+
+        try:
+            if not (os.path.isdir('result')):
+                os.makedirs(os.path.join('result'))
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                print("fail to create folder")
+                raise
+        plt.savefig('./result/'+self.file_name+'.png', dpi=100)
