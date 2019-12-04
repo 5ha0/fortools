de3e2cfacdbd11232f4b240e0c0cfe6891be971d
baeha <54804672+baeha@users.noreply.github.com>
2019-12-04 15:40:46 +0900

    Add date count

--- forlib/processing/filter.py
+++ forlib/processing/filter.py
@@ -64,3 +64,14 @@ def day_filter(key, filter_list, json_list):
 def time_sort(key, json_list):
     json_list = sorted(json_list, key=lambda list_info: (list_info[key]))
     return json_list
+
+def date_count(key, json_list):
+    result = dict()
+    for i in json_list:
+        date_value = i[key].split(' ')[0]
+        if result.get(date_value) is not None:
+            result[date_value] = result[date_value] + 1
+        else:
+            result[date_value] = 1
+    return result
+
