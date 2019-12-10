b3eec423f71e358294b9298eb92341b46b137fe2
baeha <baehayoung0214@gmail.com>
2019-12-11 06:39:05 +0900

    Add filesystem log example

---
+++ example/filesystem_log_example.py
@@ -0,0 +1,29 @@
+from fortools import *
+
+# example of filesystem log
+
+path = r'your path'
+filesys_log = FileSystemLog.file_open(path)
+
+'''
+Uncomment the comment you want to use.
+'''
+
+# # ------------------------1. Get information of filesystem log.---------------------------
+# file_syslog_info = filesys_log.get_all_info()
+# for i in file_syslog_info:
+# 	print(i)
+#
+# # ------------------------2. Get hash value of filesystem log.---------------------------
+# file_syslog_hash = filesys_log.get_hash()
+# for i in file_syslog_hash:
+# 	print(i)
+#
+# # ------------------------3. Print information of filesystem log.---------------------------
+# filesys_log.show_all_info()
+#
+# # ------------------------4. Get information of specific key.---------------------------
+# # You need to input list of keys in get_info function.
+# file_syslog_info = filesys_log.get_info(['Name', 'Parent'])
+# for i in file_syslog_info:
+# 	print(i)
