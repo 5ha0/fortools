966067ab099589c4b65ffd5b9f00483400c369df
gut27 <52551159+gut27@users.noreply.github.com>
2019-12-11 15:49:23 +0900

    Update prefetch_example.py

--- example/prefetch_example.py
+++ example/prefetch_example.py
@@ -1,8 +1,8 @@
 from fortools import *
 
 file = Prefetch.file_open(r'path')
-# Or you can use this
-# path = 'path'
+# # Or you can use this
+# path = r'path'
 # file = file_open(path)
 
 '''
@@ -10,33 +10,38 @@ Uncomment the comment you want to use.
 '''
 
 ####### 1. It shows all the parsing provided by this library at once. #######
-# information = file.show_all_info()
+# file.show_all_info()
 
 ####### 2. It handles all parsing provided by this library at once. #######
-# information = file.get_all_info()
+# prefetch = file.get_all_info()
 # # if you show 2, use print()
-# print(information)
+# print(prefetch)
 
-####### 3. You can parse only one value. #######
-# information = file.file_list()
-# # parse list [.file_name(), .last_launch_time(), .create_time(), .write_time(), .num_launch(), .file_list(), .metadata_info(), .cal_hash()]
-# print(information)
+####### 3. Used when you want to see only some of the information. #######
+# prefetch = file.get_info(['key list'])
+# # if you show 2. use print()
+# print(prefetch)
 
-####### 4. Allows you to find a specific extension file from file list. #######
-# extension = '.extension'
-# information = file.extension_filter_pf(extension)
-# print(information)
+####### 4. Use when you want to return the hash value before and after analysis of a file #######
+# prefetch = file.get_hash()
+# # if you show 2. use print()
+# print(prefetch)
 
-####### 5.If You want to make a report, Use This. #######
+####### 5. Allows you to find a specific extension file from file list. #######
+# extension = 'extension'
+# prefetch = file.extension_filter(extension)
+# # if you show 6. use print()
+# print(prefetch)
+
+####### 6.If You want to make a report, Use This. #######
 # # !caution!Use the script above first
-# # !caution!"4" does not provide the report.py function.
 # # How to make a report
-# # 1,2 use this
+# # 6-1. Docx
 # docx = DocxExport()
-# docx.table_by_json(information[0])
-# docx.save('report_name')
-# # 3 use this
-# docx = DocxExport()
-# docx.add_table(information)
-# docx.save('report_name')
+# docx.table_by_json(prefetch[0])
+# docx.save('PF')
+# # 6-2. MD
+# md = MdExport('PF')
+# md.add_table(prefetch)
+# md.save()
 
