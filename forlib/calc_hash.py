ccfb890209c5dd5696c3a3f4ba8c9d40e87dbb27
baeha <54804672+baeha@users.noreply.github.com>
2019-12-12 04:56:59 +0900

    Add type and file name

--- forlib/calc_hash.py
+++ forlib/calc_hash.py
@@ -1,7 +1,8 @@
 import hashlib
 
 
-def get_hash(path, blocksize=65536):
+def get_hash(path, type, blocksize=65536):
+    file_name = path.split('\\')[-1:]
     file = open(path, 'rb')
     hasher_md5 = hashlib.md5()
     hasher_sha1 = hashlib.sha1()
@@ -12,6 +13,8 @@ def get_hash(path, blocksize=65536):
         buf = file.read(blocksize)
     file.close()
     hash_value = dict()
+    hash_value["type"] = type
+    hash_value["name"] = file_name
     hash_value["sha1"] = hasher_sha1.hexdigest()
     hash_value["md5"] = hasher_md5.hexdigest()
     return hash_value
