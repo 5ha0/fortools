0b5faef25545c407215ff08ed67e704aa666a3b2
gut27 <52551159+gut27@users.noreply.github.com>
2019-12-11 15:50:15 +0900

    Update lnk_example.py

--- example/lnk_example.py
+++ example/lnk_example.py
@@ -1,8 +1,8 @@
 from fortools import *
 
 file = Lnk.file_open(r'path')
-# Or you ca use this
-# path = 'path'
+# # Or you can use this
+# path = r'path'
 # file = file_open(path)
 
 '''
@@ -10,23 +10,30 @@ Uncomment the comment you want to use.
 '''
 
 ####### 1. It shows all the parsing provided by this library at once. #######
-# information = file.show_all_info()
+# file.show_info()
 
 ####### 2. It handles all parsing provided by this library at once. #######
-# information = file.get_all_info()
-# # if you show 2, use print()
-# print(information)
+# lnk = file.get_all_info()
+# # if you show 2. use print()
+# print(lnk)
 
-####### 3. You can parse only one value. #######
-# information = file.lnk_creation_time()
-# # parse list [.file_attribute, .creation_time(), .access_time(), .write_time(), .lnk_creation_time(), .lnk_access_time(), .lnk_write_time(), .file_size(), icon_indes(), .show_command(), .volume(), .localbase_path(), .netbios(), .machine_id, .show_all_info(), get_all_info()]
-# print(information)
+####### 3. Used when you want to see only some of the information. #######
+# lnk = file.get_info(['key list'])
+# # if you show 2. use print()
+# print(lnk)
+
+####### 4. Use when you want to return the hash value before and after analysis of a file #######
+# lnk = file.get_hash()
+# # if you show 2. use print()
+# print(lnk)
 
 ####### 4.If You want to make a report, Use This. #######
 # # !caution!Use the script above first
-# # !caution!"3" does not provide the report.py function.
-# # How to make a report
+# # 5-1. Docx
 # docx = DocxExport()
-# docx.table_by_json(information[0])
-# docx.save('report_name')
-
+# docx.table_by_json(lnk[0])
+# docx.save('Lnk')
+# # 5-2. MD
+# md = MdExport('Lnk')
+# md.add_table(lnk)
+# md.save()
