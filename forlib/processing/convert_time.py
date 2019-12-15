99ba3d9be70fd94fd14988446b8edfb61b5458c5
gut27 <52551159+gut27@users.noreply.github.com>
2019-12-15 15:59:11 +0900

    Update convert_time.py

--- forlib/processing/convert_time.py
+++ forlib/processing/convert_time.py
@@ -1,18 +1,24 @@
 from datetime import datetime, timedelta
 
+class TimeZone()
 
-def convert_time(int_time):
-    int_time = '%016x' % int_time
-    int_time = int(int_time, 16) *0.1
-    int_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
-    return int_time
+    def _init_(self,time_zone):
+        self.__time_split = time_zone
+        self.__time_split = self.__time_split.split(':')
+        self.__time_split = int(self.__time_split)
 
-def convert_bin_time(int_time):
-    int_time = int_time*0.1
-    if int_time == 0:
-        date_time = 'Never'
-    else:
-        date_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
-    return date_time
+    def __convert_time(self, int_time):
+        int_time = '%016x' % int_time
+        int_time = int(int_time, 16) / 10.
+        int_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time) + timedelta(hours=time_zone[0], minutes=time_split[1])
+        return int_time
+
+    def convert_bin_time(int_time):
+        int_time = int_time*0.1
+        if int_time == 0:
+            date_time = 'Never'
+        else:
+            date_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
+        return date_time
     
     
