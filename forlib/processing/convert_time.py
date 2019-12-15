ece8ed0743970625d4e7c0ff5f5721aae5b5433a
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-12-15 14:45:03 +0900

    Update convert_time.py

--- forlib/processing/convert_time.py
+++ forlib/processing/convert_time.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 def convert_time(int_time):
     int_time = '%016x' % int_time
-    int_time = int(int_time, 16) / 10.
+    int_time = int(int_time, 16) *0.1
     int_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
     return int_time
 
