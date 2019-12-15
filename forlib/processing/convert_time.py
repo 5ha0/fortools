0c795d7f41763fbba41a9295df4df826cd077715
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-12-15 16:36:12 +0900

    Update convert_time.py

--- forlib/processing/convert_time.py
+++ forlib/processing/convert_time.py
@@ -9,10 +9,13 @@ class TimeZone()
 
     def __convert_time(self, int_time):
         int_time = '%016x' % int_time
-        int_time = int(int_time, 16) / 10.
-        int_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time) + timedelta(hours=time_zone[0], minutes=time_split[1])
-        return int_time
-
+        int_time = int(int_time, 16) * 0.1
+        if int_time == 0:
+            date_time = 0
+        else:
+            date_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time + timedelta(hours=time_zone[0], minutes=time_split[1]))
+            return date_time
+        
     def convert_bin_time(int_time):
         int_time = int_time*0.1
         if int_time == 0:
