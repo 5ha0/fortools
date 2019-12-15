fbf02d7338825ba6b0b75aa766232d85cac74e66
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-12-15 14:39:45 +0900

    Update convert_time.py

--- forlib/processing/convert_time.py
+++ forlib/processing/convert_time.py
@@ -1,8 +1,19 @@
 from datetime import datetime, timedelta
 
 
-def convert_time(time):
-    time = '%016x' % time
-    time = int(time, 16) / 10.
-    time = datetime(1601, 1, 1) + timedelta(microseconds=time)
-    return time
+def convert_time(int_time):
+    int_time = '%016x' % int_time
+    int_time = int(int_time, 16) / 10.
+    int_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
+    return int_time
+
+def convert_bin_time(int_time):
+    int_time = int_time*0.1
+    if int_time == 0:
+        date = 'Never'
+    else:
+        date = datetime(1601, 1, 1) + timedelta(microseconds=int_time)
+        date = date.strftime('%Y-%m-%d %H:%M:%S')
+    return date
+    
+    
