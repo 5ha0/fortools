18f0cee547898cc3b1820d783d215f98d003176b
maplejh <54804674+maplejh@users.noreply.github.com>
2019-12-16 18:56:50 +0900

    Update convert_time.py

--- forlib/processing/convert_time.py
+++ forlib/processing/convert_time.py
@@ -1,18 +1,28 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
-class TimeZone:
 
-    def __init__(self,time_zone):
-        self.__time_split = time_zone
-        self.__time_split = self.__time_split.split(':')
-        self.__time_split = int(self.__time_split)
+def set_timezone(timezone):
+    time_split = timezone.split(':')
+    minutestime=int(time_split[0])*60+int(time_split[1])
+    global mintz
+    mintz = minutestime
 
-    def __convert_time(self, int_time):
-        if type(int_time) == int:
-            int_time = int_time * 0.1
-        else:
-            int_time = '%016x' % int_time
-            int_time = int(int_time, 16) * 0.1
-            
-        date_time = datetime(1601, 1, 1) + timedelta(microseconds=int_time) + timedelta(hours=time_zone[0], minutes=time_split[1])
-        return date_time
+
+def __convert_time(int_time):
+    if type(int_time) == int:
+        int_time = int_time * 0.1
+    else:
+        int_time = '%016x' % int_time
+        int_time = int(int_time, 16) * 0.1
+
+    date_time = datetime(1601, 1, 1, tzinfo=timezone(timedelta(minutes=mintz))) + timedelta(microseconds=int_time) + timedelta(minutes=mintz)
+    return date_time
+
+
+def convert_time_ie_edge_chrome(int_time):
+    try:
+        int_time = int_time / 10.
+        int_time = datetime(1601, 1, 1, tzinfo=timezone(timedelta(minutes=mintz))) + timedelta(microseconds=int_time) + timedelta(minutes=mintz)
+        return int_time
+    except:
+        return 0
