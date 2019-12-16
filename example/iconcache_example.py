26e6d886601b7efa5402b9db180756f0ee3d84ef
gut27 <52551159+gut27@users.noreply.github.com>
2019-12-16 17:06:13 +0900

    Update iconcache_example.py

--- example/iconcache_example.py
+++ example/iconcache_example.py
@@ -17,34 +17,27 @@ Uncomment the comment you want to use.
 # # if you show 2, use print()
 # print(icon)
 
-####### 3. Used when you want to see only some of the information. #######
-# icon = file.get_info(['key list'])
-# # if you show 2. use print()
-# print(icon)
-
-####### 3. Used when you want to see only some of the information. #######
-# icon = file.get_info_by_section(['section number list'])
-# # if you show 2. use print()
-# print(icon)
-
-####### 4. Use when you want to return the hash value before and after analysis of a file #######
+####### 3. Use when you want to return the hash value before and after analysis of a file #######
 # icon = file.get_hash()
 # # if you show 2. use print()
 # print(icon)
 
-####### 5.Allows you to find a specific extension file from all sections. #######
-# extension = 'extension'
-# icon = file.extension_filter(extension)
-# # if you show 6. use print()
-# print(icon)
+####### 4. For the convenience of users, only the information of the desired section is displayed. #######
+# icon = file.Favorite.show_info_by_section(['section number list'])
 
-####### 6.If You want to make a report, Use This. #######
+####### 5.Shows the extension information for all sections of the path information and the number of each extension. #######
+# icon = file.Favorite.show_kind_of_extension()
+
+####### 6.The path information in all sections shows the drive type information and the number of each drive type. #######
+# icon = file.Favorite.show_kind_of_drivetype()
+
+####### 7.If You want to make a report, Use This. #######
 # # !caution!Use the script above first
-# # 6-1. Docx
+# # 7-1. Docx
 # docx = DocxExport()
 # docx.table_by_json(icon[i])
 # docx.save('ICON')
-# # 6-2. MD
+# # 7-2. MD
 # md = MdExport('ICON')
 # md.add_table(info)
 # md.save()
