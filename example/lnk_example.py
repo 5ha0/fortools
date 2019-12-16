4d4e169d73053231dee15930fa3a7bab699e06f9
gut27 <52551159+gut27@users.noreply.github.com>
2019-12-16 17:13:20 +0900

    Update lnk_example.py

--- example/lnk_example.py
+++ example/lnk_example.py
@@ -17,23 +17,18 @@ Uncomment the comment you want to use.
 # # if you show 2. use print()
 # print(lnk)
 
-####### 3. Used when you want to see only some of the information. #######
-# lnk = file.get_info(['key list'])
-# # if you show 2. use print()
-# print(lnk)
-
-####### 4. Use when you want to return the hash value before and after analysis of a file #######
+####### 3. Use when you want to return the hash value before and after analysis of a file #######
 # lnk = file.get_hash()
 # # if you show 2. use print()
 # print(lnk)
 
 ####### 4.If You want to make a report, Use This. #######
 # # !caution!Use the script above first
-# # 5-1. Docx
+# # 4-1. Docx
 # docx = DocxExport()
 # docx.table_by_json(lnk[0])
 # docx.save('Lnk')
-# # 5-2. MD
+# # 4-2. MD
 # md = MdExport('Lnk')
 # md.add_table(lnk)
 # md.save()
