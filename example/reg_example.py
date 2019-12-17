0b7552c43b0cf1b17b7eb0a7fd81732dab4d2cf2
liberte97 <35121677+liberte97@users.noreply.github.com>
2019-12-17 18:29:43 +0900

    Update reg_example.py

--- example/reg_example.py
+++ example/reg_example.py
@@ -30,37 +30,41 @@ info = reg_file1.find_value("Software\\Microsoft\\Windows\\CurrentVersion\\Explo
 
 # ------------------- NTUSER.DAT ------------------------------------------
 # # 1. Bring up a list of recently run documents.
-# info = reg_file1.Favorite.NTAnalysis.get_recent_docs()
+# info = reg_file1.Favorite.NTUSER.get_recent_docs()
 # # 2. Check the MRU cache information.
-# info = reg_file1.Favorite.NTAnalysis.get_recent_MRU()
+# info = reg_file1.Favorite.NTUSER.get_recent_MRU()
 # # 3. Check the MS document and outlook information you ran.
-# info = reg_file1.Favorite.NTAnalysis.get_ms_office()
+# info = reg_file1.Favorite.NTUSER.get_ms_office()
 # # 4. Get information such as the number of times and time of recently executed files.
-# info = reg_file1.Favorite.NTAnalysis.get_userassist()
+# info = reg_file1.Favorite.NTUSER.get_userassist()
+# # 5. Get Hwp file information
+# info = reg_file1.Favorite.NTUSER.get_HWP()
+# # 6. Check url accessed from IE
+# info = reg_file1.Favorite.NTUSER.get_IE_visit()
 
 # ---------------------SYSTEM -------------------------------------------
 # # 1. Get computer basic information.
-# info = reg_file2.Favorite.SYSAnalysis.get_computer_info()
+# info = reg_file2.Favorite.SYSTEM.get_computer_info()
 # # 2. Get the USB trace and show it.
-# info = reg_file2.Favorite.SYSAnalysis.get_USB()
+# info = reg_file2.Favorite.SYSTEM.get_USB()
 # # 3. Get the set time zone value.
-# info = reg_file2.Favorite.SYSAnalysis.get_timezone()
+# info = reg_file2.Favorite.SYSTEM.get_timezone()
 # # 4. Recall network basic information.
-# info = reg_file2.Favorite.SYSAnalysis.get_network_info()
+# info = reg_file2.Favorite.SYSTEM.get_network_info()
 
 # --------------------- SOFTWARE ---------------------
 # # 1. Get basic information of computer OS.
-# info = reg_file3.Favorite.SWAnalysis.get_info()
+# info = reg_file3.Favorite.SOFTWARE.get_info()
 # # 2. Get network card information.
-# info = reg_file3.Favorite.SWAnalysis.get_network_info()
+# info = reg_file3.Favorite.SOFTWARE.get_network_info()
 
 # --------------------- SAM ---------------------
 # # 1. Get the information of the last logged in user.
-# info = reg_file4.Favorite.SAMAnalysis.last_login()
+# info = reg_file4.Favorite.SAM.last_login()
 # # 2. Get user basic information.
-# info = reg_file4.Favorite.SAMAnalysis.user_name()
+# info = reg_file4.Favorite.SAM.user_name()
 # # 3. Get user details.
-# info = reg_file4.Favorite.SAMAnalysis.user_info()
+# info = reg_file4.Favorite.SAM.user_info()
 
 for i in info:
     print(i)
