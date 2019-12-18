0e75c8af32503a0d4a8c93ca2e5b7992655c4305
maplejh <54804674+maplejh@users.noreply.github.com>
2019-12-18 23:55:56 +0900

    Update winsearch_analysis.py

--- forlib/processing/winsearch_analysis.py
+++ forlib/processing/winsearch_analysis.py
@@ -2,19 +2,22 @@ import pyesedb
 import binascii
 from datetime import *
 import forlib.calc_hash as calc_hash
+import forlib.processing.convert_time as convert_time
 
 class WinSearchAnalysis:
 
     def __init__(self, file, path, hash_v):
-        self._result = []
+        self.winsearch_list = []
         self.__file = file
-        self.__json = self.__parse()
+        if self.__file==-1:
+            self.__winsearch_list==""
+        else:
+            self.__parse()
         self.__hash_value = [hash_v]
         self.__path = path
         self.__cal_hash()
 
     def __parse(self):
-        result = []
         edb_data_table = self.__file.get_table_by_name("SystemIndex_0A")
         no = 0
         for row in edb_data_table.records:
@@ -22,22 +25,23 @@ class WinSearchAnalysis:
             mkdict = dict()
             mkdict["index"] = no
             mkdict["type"] = "window search.edb"
+            mkdict["timezone"]=convert_time.convert_time(0).strftime("%Z")
             mkdict["DocID"] = row.get_value_data_as_integer(0)
             mkdict["FileName"] = row.get_value_data_as_string(31).split('\\')[-1]
             if row.get_value_data(5) is None:
-                mkdict["System_DateModified"] = ""
+                mkdict["System_DateModified"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
             else:
-                mkdict["System_DateModified"] = int2date(int(bytes.decode(binascii.hexlify(row.get_value_data(5))), 16))
+                mkdict["System_DateModified"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(5))), 16)).strftime("%Y-%m-%d %H:%M:%S")
 
             if row.get_value_data(6) is None:
-                mkdict["System_DateCreated"] = ""
+                mkdict["System_DateCreated"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
             else:
-                mkdict["System_DateCreated"] = int2date(int(bytes.decode(binascii.hexlify(row.get_value_data(6))), 16))
+                mkdict["System_DateCreated"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(6))), 16)).strftime("%Y-%m-%d %H:%M:%S")
 
             if row.get_value_data(7) is None:
-                mkdict["System_DateAccessed"] = ""
+                mkdict["System_DateAccessed"] = convert_time.convert_time(0).strftime("%Y-%m-%d %H:%M:%S")
             else:
-                mkdict["System_DateAccessed"] = int2date(int(bytes.decode(binascii.hexlify(row.get_value_data(7))), 16))
+                mkdict["System_DateAccessed"] = convert_time.convert_time(int(bytes.decode(binascii.hexlify(row.get_value_data(7))), 16)).strftime("%Y-%m-%d %H:%M:%S")
 
             mkdict["System_IsFolder"] = int(bytes.decode(binascii.hexlify(row.get_value_data(19))), 16)
 
@@ -51,8 +55,9 @@ class WinSearchAnalysis:
             if row.get_value_data(193) is None:
                 mkdict["System_Search_AutoSummary"] = ""
             else:
-                data1 = row.get_value_data(193)
-                data = bytes.decode(binascii.hexlify(row.get_value_data(193)))
+                mkdict["System_Search_AutoSummary"] = ""
+                # data1 = row.get_value_data(193)
+                # data = bytes.decode(binascii.hexlify(row.get_value_data(193)))
 
             #     mkdict["System_Search_AutoSummary"]=bytes.fromhex(data).decode("utf-16")
             mkdict["System_ItemTypeText"] = row.get_value_data_as_string(253)
@@ -60,26 +65,17 @@ class WinSearchAnalysis:
             mkdict["System_ItemPathDisplayNarrow"] = row.get_value_data_as_string(284)
             mkdict["System_FileName"] = row.get_value_data_as_string(363)
 
-            result.append(mkdict)
-        return result
+            self.winsearch_list.append(mkdict)
 
     def show_all_info(self):
-        for i in self.__json:
+        for i in self.winsearch_list:
             print(i)
 
     def get_all_info(self):
-        return self.__json
+        return self.winsearch_list
 
     def __cal_hash(self):
         self.__hash_value.append(calc_hash.get_hash(self.__path, 'after'))
 
     def get_hash(self):
         return self.__hash_value
-
-
-def int2date(date_time):
-    # 1601년 1월 1일부터
-    from_date = datetime(1601, 1, 1)
-    passing_time = timedelta(microseconds=(date_time * 0.1))
-    get_date = from_date + passing_time
-    return get_date.strftime("%Y-%m-%d %H:%M:%S")
